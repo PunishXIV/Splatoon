3e45f90f2169d5b9ead650581a291eba83d4053e
Limiana <5073202+Limiana@users.noreply.github.com>
2023-11-29 22:57:44 +0300

    commit all changes

---
+++ SplatoonScripts/Generic/OpenPFCreation.cs
@@ -0,0 +1,52 @@
+﻿using Dalamud.Game.Addon.Lifecycle;
+using Dalamud.Game.Addon.Lifecycle.AddonArgTypes;
+using ECommons.Automation;
+using ECommons.DalamudServices;
+using ECommons.Throttlers;
+using FFXIVClientStructs.FFXIV.Component.GUI;
+using Splatoon.SplatoonScripting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SplatoonScriptsOfficial.Generic
+{
+    public unsafe class OpenPFCreation : SplatoonScript
+    {
+        public override HashSet<uint> ValidTerritories => new();
+        bool OpenCreate = false;
+
+        public override void OnEnable()
+        {
+            Svc.Commands.AddHandler("/createpf", new(OpenPF));
+            Svc.AddonLifecycle.RegisterListener(AddonEvent.PostSetup, "LookingForGroup", OpenRecruitment);
+        }
+
+        private void OpenRecruitment(AddonEvent type, AddonArgs args)
+        {
+            if (OpenCreate)
+            {
+                OpenCreate = false;
+                Callback.Fire((AtkUnitBase*)args.Addon, true, 14);
+            }
+        }
+
+        private void OpenPF(string command, string arguments)
+        {
+            if (EzThrottler.Throttle("CreatePF"))
+            {
+                Chat.Instance.SendMessage("/pfinder");
+                OpenCreate = true;
+            }
+        }
+
+        public override void OnDisable()
+        {
+            Svc.AddonLifecycle.UnregisterListener(AddonEvent.PostSetup, "LookingForGroup", OpenRecruitment);
+            Svc.Commands.RemoveHandler("/createpf");
+            OpenCreate = false;
+        }
+    }
+}

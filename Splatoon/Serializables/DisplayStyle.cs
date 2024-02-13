5af5ad2f17f7ff724b6dba5b0f34cb8bddf7e3f3
sourpuh <150582765+sourpuh@users.noreply.github.com>
2024-02-12 20:59:31 -0800

    Delete dead code

--- Splatoon/Serializables/DisplayStyle.cs
+++ Splatoon/Serializables/DisplayStyle.cs
@@ -7,27 +7,5 @@
         public uint originFillColor = originFillColor;
         public uint endFillColor = endFillColor;
         public bool filled = filled;
-
-        public uint fillColor(float amount)
-        {
-            return Lerp(originFillColor, endFillColor, amount);
-        }
-
-        public uint animatedOriginFillColor(float animatePercent)
-        {
-            if (animatePercent < 0.5)
-            {
-                return Lerp(originFillColor, endFillColor, animatePercent / 0.5f);
-            }
-            return endFillColor;
-        }
-        public uint animatedEndFillColor(float animatePercent)
-        {
-            if (animatePercent > 0.5)
-            {
-                return Lerp(originFillColor, endFillColor, (animatePercent - 0.5f) / 0.5f);
-            }
-            return originFillColor;
-        }
     }
 }

6b109c438bb64a7ffdb249f933b0cdf030c7b832
sourpuh <150582765+sourpuh@users.noreply.github.com>
2024-02-29 22:49:14 -0800

    Add configuration for alpha blend mode

---
+++ Splatoon/Serializables/AlphaBlendMode.cs
@@ -0,0 +1,29 @@
+﻿using ECommons.LanguageHelpers;
+
+namespace Splatoon.Serializables;
+
+public enum AlphaBlendMode
+{
+    None = 0,
+    Add = 1,
+    Max = 2,
+}
+
+public static class AlphaBlendModes
+{
+    public static readonly string[] Names =
+    [
+        "None".Loc(),
+        "Add".Loc(),
+        "Maximum".Loc(),
+    ];
+
+    public static readonly string[] Tooltips =
+    [
+        "Overlapping elements are not blended.\nWhatever is drawn last overwrites anything it overlaps.",
+        "Overlapping elements are blended by adding their alpha values.\nThis makes it easier to see areas where elements overlap.",
+        "Overlapping elements are blended using the most opaque alpha.\nThis makes it easier to see through overlapping areas.",
+    ];
+
+    public static readonly AlphaBlendMode[] Values = (AlphaBlendMode[])Enum.GetValues(typeof(AlphaBlendMode));
+}

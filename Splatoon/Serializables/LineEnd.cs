d0f87735edca1e298ab1910606ca650ebd0900aa
sourpuh <150582765+sourpuh@users.noreply.github.com>
2024-03-04 22:55:13 -0800

    Add option to draw arrows on lines and tethers

---
+++ Splatoon/Serializables/LineEnd.cs
@@ -0,0 +1,26 @@
+﻿using ECommons.LanguageHelpers;
+
+namespace Splatoon.Serializables;
+
+public enum LineEnd
+{
+    None = 0,
+    Arrow = 1,
+}
+
+public static class LineEnds
+{
+    public static readonly string[] Names =
+    [
+        "None".Loc(),
+        "Arrow".Loc(),
+    ];
+
+    public static readonly string[] Tooltips =
+    [
+        "No line end caps.".Loc(),
+        "Arrow pointing out.".Loc(),
+    ];
+
+    public static readonly LineEnd[] Values = (LineEnd[])Enum.GetValues(typeof(LineEnd));
+}

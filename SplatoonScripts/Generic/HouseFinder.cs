e0a43c9ab91a4bf7027b089f879aa311f4a6ebee
Limiana <5073202+Limiana@users.noreply.github.com>
2024-03-21 19:45:00 +0300

    .net 8 update

--- SplatoonScripts/Generic/HouseFinder.cs
+++ SplatoonScripts/Generic/HouseFinder.cs
@@ -1,7 +1,9 @@
 ﻿using Dalamud.Game.Text.SeStringHandling;
+using Dalamud.Game.Text.SeStringHandling.Payloads;
 using ECommons;
 using ECommons.Automation;
 using ECommons.ChatMethods;
+using ECommons.DalamudServices;
 using ECommons.ImGuiMethods;
 using ECommons.Logging;
 using ECommons.MathHelpers;
@@ -10,6 +12,7 @@ using ECommons.UIHelpers;
 using FFXIVClientStructs.FFXIV.Client.UI;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 using ImGuiNET;
+using Lumina.Excel.GeneratedSheets;
 using Splatoon.SplatoonScripting;
 using System;
 using System.Collections.Generic;
@@ -28,11 +31,15 @@ public unsafe class HouseFinder : SplatoonScript
     int AutoScanValue = 0;
     bool AutoScan = false;
     int StopAt = 30;
+    int LastScannedWard = -1;
+    bool Enable = true;
 
     public override void OnUpdate()
     {
-        if(ToFind != "" || PriceMax > 0)
+        if (!Enable) return;
+        if(ToFind != "" || PriceMax > 0 || Sizes.Any() || Tags.Any())
         {
+            var lscw = LastScannedWard;
             {
                 var playedSound = false;
                 if (GenericHelpers.TryGetAddonByName<AtkUnitBase>("HousingSelectBlock", out var addon) && GenericHelpers.IsAddonReady(addon))
@@ -41,10 +48,14 @@ public unsafe class HouseFinder : SplatoonScript
                     for (int i = 0; i < reader.Houses.Count; i++)
                     {
                         var x = reader.Houses[i];
-                        if((ToFind != "" && x.HouseName.Contains(ToFind, StringComparison.OrdinalIgnoreCase)) || (int.TryParse(x.HouseName.Replace(",", "").Split(" ")[0], out var price) && price.InRange(PriceMin, PriceMax)))
+                        if((ToFind != "" && x.HouseName.Contains(ToFind, StringComparison.OrdinalIgnoreCase)) || Sizes.Any() || Tags.Any() || (int.TryParse(x.HouseName.Replace(",", "").Split(" ")[0], out var price) && price.InRange(PriceMin, PriceMax)))
                         {
-                            if(Throttler.Throttle($"Notify{x.HouseName} / {reader.WardNumber} / {i}", 10000))
+                            if (Sizes.Count > 0 && !Sizes.Contains(x.PlotSize)) continue;
+                            if (Tags.Count > 0 && !Tags.ContainsAny(x.Tags)) continue;
+                            if(ExcludeDenied && x.IsEntryDenied) continue;
+                            if (LastScannedWard != reader.WardNumber)
                             {
+                                lscw = reader.WardNumber;
                                 ChatPrinter.Red($"Found {x.HouseName} ward {reader.WardNumber + 1}, plot {i + 1}");
                                 if (!playedSound)
                                 {
@@ -80,21 +91,61 @@ public unsafe class HouseFinder : SplatoonScript
                     }
                 }
             }
+            LastScannedWard = lscw;
         }
     }
 
+    public enum HouseSize { Small, Medium, Large }
+    public const string SizeLetterS = "";
+    public const string SizeLetterM = "";
+    public const string SizeLetterL = "";
+
+    public List<uint> Tags = new();
+    public List<HouseSize> Sizes = new();
+    public bool ExcludeDenied = false;
     public override void OnSettingsDraw()
     {
+        ImGui.Checkbox("Enable", ref Enable);
+        ImGui.SameLine();
+        if(ImGui.Button("Reset scan"))
+        {
+            LastScannedWard = -1;
+        }
         ImGui.InputText($"Partial house name", ref ToFind, 50);
         ImGui.InputInt($"Price min", ref PriceMin);
         ImGui.InputInt($"Price max", ref PriceMax);
         if(ImGui.Checkbox("Auto scan", ref AutoScan))
         {
+            Svc.Chat.Print($"Auto scan begun");
+            Throttler.Throttle("AutoScanDelay", 100, true);
+            LastScannedWard = -1;
             AutoScanValue = 0;
         }
         ImGui.SameLine();
         ImGuiEx.Text($"{AutoScanValue}");
         ImGui.InputInt($"Stop autoscan at", ref StopAt);
+        if(ImGui.CollapsingHeader("Select tags"))
+        {
+            foreach (var x in Svc.Data.GetExcelSheet<HousingAppeal>()!)
+            {
+                if (x.Icon != 0)
+                {
+                    if (ThreadLoadImageHandler.TryGetIconTextureWrap(x.Icon, false, out var tex))
+                    {
+                        ImGui.Image(tex.ImGuiHandle, new System.Numerics.Vector2(24));
+                        ImGui.SameLine();
+                    }
+                    ImGuiEx.CollectionCheckbox($"{x.Tag}", x.Icon, Tags);
+                }
+            }
+        }
+        ImGuiEx.TextV("Size:");
+        foreach(var x in Enum.GetValues<HouseSize>())
+        {
+            ImGui.SameLine();
+            ImGuiEx.CollectionCheckbox($"{x}", x, Sizes);
+        }
+        ImGui.Checkbox($"Exclude inaccessible", ref ExcludeDenied);
         if (ImGui.CollapsingHeader("Debug"))
         {
             if (GenericHelpers.TryGetAddonByName<AtkUnitBase>("HousingSelectBlock", out var addon) && GenericHelpers.IsAddonReady(addon))
@@ -102,7 +153,7 @@ public unsafe class HouseFinder : SplatoonScript
                 var reader = new ReaderHousingSelectBlock(addon);
                 foreach(var x in reader.Houses)
                 {
-                    ImGuiEx.Text($"{x.HouseName} {(int.TryParse(x.HouseName.Replace(",", "").Split(" ")[0], out var price)?price.ToString():"-")}");
+                    ImGuiEx.Text($"{x.HouseName} {(int.TryParse(x.HouseName.Replace(",", "").Split(" ")[0], out var price)?price.ToString():"-")} [{x.EntryPermission} / {x.Unk3}]");
                 }
                 ImGuiEx.Text($"{reader.Houses.Count}");
             }
@@ -126,13 +177,30 @@ public unsafe class HouseFinder : SplatoonScript
             {
             }
 
-            public uint Unk0 => this.ReadUInt(0) ?? 0;
-            public SeString PlotSize => this.ReadSeString(1);
+            public uint EntryPermission => this.ReadUInt(0) ?? 0;
+            public bool IsEntryDenied => this.EntryPermission.EqualsAny<uint>(0, 4);
             public string HouseName => this.ReadString(2);
             public uint Unk3 => this.ReadUInt(3) ?? 0;
-            public uint Unk4 => this.ReadUInt(4) ?? 0;
-            public uint Unk5 => this.ReadUInt(5) ?? 0;
-            public uint Unk6 => this.ReadUInt(6) ?? 0;
+            public uint[] Tags => new uint[] { this.ReadUInt(4) ?? 0, this.ReadUInt(5) ?? 0, this.ReadUInt(6) ?? 0 };
+            public HouseSize PlotSize
+            {
+                get
+                {
+                    var plotSizeRaw = this.ReadSeString(1);
+                    foreach(var x in plotSizeRaw.Payloads)
+                    {
+                        if(x is TextPayload text)
+                        {
+                            if (text.Text != null)
+                            {
+                                if (text.Text.Contains($"{SizeLetterS}{SizeLetterM}")) return HouseSize.Large;
+                                if (text.Text.Contains($"{SizeLetterM}{SizeLetterL}")) return HouseSize.Small;
+                            }
+                        }
+                    }
+                    return HouseSize.Medium;
+                }
+            }
         }
     }
 }

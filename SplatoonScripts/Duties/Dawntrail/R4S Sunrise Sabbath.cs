6c69d869cd66109fb9cd3cb408af83b9bc54c87e
Limiana <5073202+Limiana@users.noreply.github.com>
2024-08-12 03:40:06 +0300

    adjust vfx timing and add extra check

--- SplatoonScripts/Duties/Dawntrail/R4S Sunrise Sabbath.cs
+++ SplatoonScripts/Duties/Dawntrail/R4S Sunrise Sabbath.cs
@@ -22,7 +22,7 @@ namespace SplatoonScriptsOfficial.Duties.Dawntrail;
 public class R4S_Sunrise_Sabbath : SplatoonScript
 {
     public override HashSet<uint>? ValidTerritories { get; } = [1232];
-    public override Metadata? Metadata => new(4, "NightmareXIV");
+    public override Metadata? Metadata => new(5, "NightmareXIV");
     uint DebuffYellow = 4000;
     uint DebuffBlue = 4001;
     string GunYellow = "vfx/common/eff/m0888_stlp04_c0t1.avfx";
@@ -123,7 +123,7 @@ public class R4S_Sunrise_Sabbath : SplatoonScript
                         var gun = guns[i];
                         if(AttachedInfo.TryGetVfx(gun, out var vfx))
                         {
-                            var req = vfx?.Where(x => x.Key.EqualsAny(this.GunBlue, this.GunYellow) && x.Value.AgeF < 15f).OrderBy(x => x.Value.AgeF).FirstOrDefault().Key;
+                            var req = vfx?.Where(x => x.Key.EqualsAny(this.GunBlue, this.GunYellow) && x.Value.AgeF < 10f).OrderBy(x => x.Value.AgeF).FirstOrDefault().Key;
                             if(req == this.GunBlue)
                             {
                                 if(showAoe)

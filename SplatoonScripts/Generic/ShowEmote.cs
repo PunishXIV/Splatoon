37bb33dd7f195200a6feb17e2675cde0576b3591
Limiana <5073202+Limiana@users.noreply.github.com>
2024-08-21 18:52:56 +0300

    Update script

--- SplatoonScripts/Generic/ShowEmote.cs
+++ SplatoonScripts/Generic/ShowEmote.cs
@@ -1,81 +1,81 @@
-﻿using Dalamud.Data;
-using Dalamud.Game.ClientState.Objects;
-using Dalamud.Game.Gui;
+﻿using Dalamud.Game.ClientState.Objects.SubKinds;
+using Dalamud.Game.ClientState.Objects.Types;
 using Dalamud.Hooking;
-using Dalamud.Logging;
-using Dalamud.Memory;
-using Dalamud.Plugin;
 using Dalamud.Utility.Signatures;
 using ECommons;
 using ECommons.Configuration;
 using ECommons.DalamudServices;
 using ECommons.DalamudServices.Legacy;
+using ECommons.EzHookManager;
 using ECommons.GameFunctions;
-using ECommons.ImGuiMethods;
+using ECommons.GameHelpers;
+using ECommons.Logging;
+using FFXIVClientStructs.FFXIV.Client.Game.Control;
+using FFXIVClientStructs.FFXIV.Client.Game.Event;
 using FFXIVClientStructs.FFXIV.Client.Game.Object;
+using FFXIVClientStructs.FFXIV.Client.UI.Agent;
 using ImGuiNET;
 using Lumina.Excel.GeneratedSheets;
 using Splatoon.SplatoonScripting;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+#nullable disable
 
-namespace SplatoonScriptsOfficial.Generic
+namespace SplatoonScriptsOfficial.Generic;
+
+public unsafe class ShowEmote : SplatoonScript
 {
-    public unsafe class ShowEmote : SplatoonScript
+    public override HashSet<uint> ValidTerritories => null;
+    public override Metadata Metadata => new(3, "NightmareXIV");
+
+    private delegate long OnEmoteFuncDelegate(IntPtr a1, GameObject* source, ushort emoteId, GameObjectId targetId, long a5);
+    [Signature("40 53 56 41 54 41 57 48 83 EC 38", DetourName = nameof(OnEmoteFuncDetour))]
+    private Hook<OnEmoteFuncDelegate> OnEmoteFuncHook;
+
+    public override void OnEnable()
     {
-        public override HashSet<uint> ValidTerritories => new();
-        public override Metadata? Metadata => new(2, "NightmareXIV");
+        SignatureHelper.Initialise(this);
+        OnEmoteFuncHook?.Enable();
+    }
 
-        delegate long OnEmoteFuncDelegate(IntPtr a1, GameObject* source, ushort emoteId, long targetId, long a5);
-        [Signature("48 89 5C 24 ?? 48 89 6C 24 ?? 48 89 74 24 ?? 48 89 7C 24 ?? 41 56 48 83 EC 30 4C 8B 74 24 ?? 48 8B D9", DetourName = nameof(OnEmoteFuncDetour))]
-        Hook<OnEmoteFuncDelegate>? OnEmoteFuncHook;
+    public override void OnDisable()
+    {
+        Svc.Commands.RemoveHandler("/playemote");
+        OnEmoteFuncHook?.Dispose();
+    }
 
-        public override void OnEnable()
+    public override void OnSettingsDraw()
+    {
+        ImGui.Checkbox("Display emotes on all targets", ref Controller.GetConfig<Config>().ShowOnOthers);
+    }
+
+    private long OnEmoteFuncDetour(IntPtr a1, GameObject* source, ushort emoteId, GameObjectId targetId, long a5)
+    {
+        try
         {
-            SignatureHelper.Initialise(this);
-            OnEmoteFuncHook?.Enable();
-        }
-
-        public override void OnDisable()
-        {
-            OnEmoteFuncHook?.Disable();
-            OnEmoteFuncHook?.Dispose();
-        }
-
-        public override void OnSettingsDraw()
-        {
-            ImGui.Checkbox("Display emotes on all targets", ref this.Controller.GetConfig<Config>().ShowOnOthers);
-        }
-
-        long OnEmoteFuncDetour(IntPtr a1, GameObject* source, ushort emoteId, long targetId, long a5)
-        {
-            try
+            if(targetId == Svc.ClientState.LocalPlayer?.EntityId)
             {
-                if (targetId == Svc.ClientState.LocalPlayer?.EntityId)
-                {
-                    var emoteName = Svc.Data.GetExcelSheet<Emote>()?.GetRow(emoteId)?.Name;
-                    Svc.Chat.Print($">> {GenericHelpers.Read(source->Name)} uses {emoteName} on you.");
-                }
-                else if (this.Controller.GetConfig<Config>().ShowOnOthers)
-                {
-                    var emoteName = Svc.Data.GetExcelSheet<Emote>()?.GetRow(emoteId)?.Name;
-                    var target = Svc.Objects.FirstOrDefault(x => (ulong)x.Struct()->GetGameObjectId() == (ulong)targetId);
-                    Svc.Chat.Print($">> {GenericHelpers.Read(source->Name)} uses {emoteName}" + (target != null ? $" on {target.Name}" : ""));
-                }
+                var emoteName = Svc.Data.GetExcelSheet<Emote>()?.GetRow(emoteId)?.Name;
+                Svc.Chat.Print($">> {GenericHelpers.Read(source->Name)} uses {emoteName} on you.");
             }
-            catch (Exception e)
+            else if(Controller.GetConfig<Config>().ShowOnOthers)
             {
-                Svc.Chat.Print($"{e.Message}\n{e.StackTrace}");
+                var emoteName = Svc.Data.GetExcelSheet<Emote>()?.GetRow(emoteId)?.Name;
+                var target = Svc.Objects.FirstOrDefault(x => (ulong)x.Struct()->GetGameObjectId() == (ulong)targetId);
+                Svc.Chat.Print($">> {GenericHelpers.Read(source->Name)} uses {emoteName}" + (target != null ? $" on {target.Name}" : ""));
             }
-            return OnEmoteFuncHook!.Original(a1, source, emoteId, targetId, a5);
         }
-
-        public class Config : IEzConfig
+        catch(Exception e)
         {
-            public bool ShowOnOthers = false;
+            Svc.Chat.Print($"{e.Message}\n{e.StackTrace}");
         }
+        return OnEmoteFuncHook!.Original(a1, source, emoteId, targetId, a5);
+    }
+
+    public class Config : IEzConfig
+    {
+        public bool ShowOnOthers = false;
     }
 }

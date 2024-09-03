32d88077114832ba3a30b7215ce4474c55f5737c
Limiana <5073202+Limiana@users.noreply.github.com>
2024-09-03 05:55:45 +0300

    Update tests

---
+++ SplatoonScripts/Tests/GenericTest6.cs
@@ -0,0 +1,35 @@
+﻿using ECommons.ImGuiMethods;
+using ECommons.Logging;
+using ECommons.SimpleGui;
+using Splatoon.SplatoonScripting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SplatoonScriptsOfficial.Tests;
+public class GenericTest6 : SplatoonScript
+{
+    public override HashSet<uint>? ValidTerritories { get; } = null;
+
+    public override Metadata? Metadata => new(2);
+
+    public override void OnScriptUpdated(uint previousVersion)
+    {
+        if(previousVersion < 2)
+        {
+            PluginLog.Information("Updated");
+            new PopupWindow(() =>
+            {
+                ImGuiEx.Text($"""
+                    Warning: Splatoon Script 
+                    {this.InternalData.Name}
+                    was updated.
+                    If you were using Sidewise Spark related functions,
+                    you must reconfigure the script.
+                    """);
+            });
+        }
+    }
+}

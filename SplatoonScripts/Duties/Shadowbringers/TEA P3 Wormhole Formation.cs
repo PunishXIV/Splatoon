b5bac4ab08d3da3a071346abd0644270d57a2e0f
Garume <80187947+Garume@users.noreply.github.com>
2024-09-17 23:20:39 +0900

    Add TEA scripts (#165)
    * [add] TEA Scripts
    
    * [fix] Wormhole Formation
    
    * [fix] version up wormhole script
    
    * [fix] Add process to end in wormhole formation
    
    * [add] TEA p4 fate projection alpha script
    
    * [fix] a bug that would not display correctly
    
    * [add] tea fate projection beta
    
    * [add] TEA Scripts
    
    * [fix] Wormhole Formation
    
    * [fix] version up wormhole script
    
    * [fix] Add process to end in wormhole formation
    
    * [add] TEA p4 fate projection alpha script
    
    * [fix] a bug that would not display correctly
    
    * [add] tea fate projection beta
    
    * Update SplatoonScripts/update.csv
    
    * [add] tea script description
    
    * Delete SplatoonScripts/Duties/Shadowbringers/TEA P2 The Final Word.cs
    
    * Update SplatoonScripts/update.csv
    
    ---------
    
    Co-authored-by: GitHub Action <noreply@nightmarexiv.com>

---
+++ SplatoonScripts/Duties/Shadowbringers/TEA P3 Wormhole Formation.cs
@@ -0,0 +1,200 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using Dalamud.Interface.Components;
+using ECommons;
+using ECommons.Configuration;
+using ECommons.DalamudServices;
+using ECommons.ImGuiMethods;
+using ECommons.MathHelpers;
+using ECommons.Schedulers;
+using ImGuiNET;
+using Splatoon;
+using Splatoon.Memory;
+using Splatoon.SplatoonScripting;
+
+namespace SplatoonScriptsOfficial.Duties.Shadowbringers;
+
+public class TEA_P3_Wormhole_Formation : SplatoonScript
+{
+    private const uint ChakramCastId = 18517;
+    private static readonly uint[] WormholeDataIds = [2007519, 2007520, 2007521];
+
+    private readonly Dictionary<int, Vector2[]> _baitPositions = new()
+    {
+        { 1, [new Vector2(86.5f, 86f), new Vector2(86.5f, 86f), new Vector2(81f, 99f), new Vector2(90f, 97f)] },
+        { 2, [new Vector2(113.5f, 86f), new Vector2(113.5f, 86f), new Vector2(119f, 99f), new Vector2(110f, 103f)] },
+        { 3, [new Vector2(87, 113), new Vector2(87f, 113f), new Vector2(81f, 101f), new Vector2(81f, 101f)] },
+        { 4, [new Vector2(113, 113), new Vector2(113f, 113f), new Vector2(119f, 101f), new Vector2(119f, 101f)] },
+        { 5, [new Vector2(84.45f, 89.65f), new Vector2(82f, 96f), new Vector2(86.5f, 86f), new Vector2(81f, 99f)] },
+        { 6, [new Vector2(115.55f, 89.65f), new Vector2(118, 104f), new Vector2(113.5f, 86f), new Vector2(119f, 99f)] },
+        { 7, [new Vector2(83, 93), new Vector2(81.5f, 100f), new Vector2(85f, 93f), new Vector2(86.5f, 114f)] },
+        { 8, [new Vector2(117, 93), new Vector2(118.5f, 100f), new Vector2(115f, 107f), new Vector2(113.5f, 114f)] }
+    };
+
+    private readonly List<List<int>> _invertApplyIndex =
+    [
+        [],
+        [5, 6],
+        [7, 8],
+        [1, 2]
+    ];
+
+    private TickScheduler? _chakramScheduler;
+    private int _currentPhase;
+    private bool _isStartWormholeFormation;
+    private int _myNumber;
+    private bool _shouldInvert;
+    private int _wormholeChangedCount;
+
+    public override HashSet<uint>? ValidTerritories => [887];
+    public override Metadata? Metadata => new(3, "Garume");
+
+    private Config C => Controller.GetConfig<Config>();
+
+    public override void OnSetup()
+    {
+        for (var i = 1; i <= 8; i++)
+        {
+            var element = new Element(0)
+            {
+                radius = 0.35f,
+                overlayVOffset = 2f,
+                overlayFScale = 2f
+            };
+
+            Controller.RegisterElement($"Bait{i}", element, true);
+        }
+    }
+
+    public override void OnMessage(string message)
+    {
+        if (message.Contains("アレキサンダー・プライムの「次元断絶のマーチ」")) _isStartWormholeFormation = true;
+    }
+
+    public override void OnStartingCast(uint source, uint castId)
+    {
+        if (!_isStartWormholeFormation) return;
+        if (castId == ChakramCastId) _chakramScheduler ??= new TickScheduler(() => _currentPhase = 1, 5700);
+    }
+
+    public override void OnVFXSpawn(uint target, string vfxPath)
+    {
+        if (!vfxPath.StartsWith("vfx/lockon/eff/m0361trg_a")) return;
+        if (!AttachedInfo.VFXInfos.TryGetValue(Svc.ClientState.LocalPlayer.Address, out var info)) return;
+        if (info.OrderBy(x => x.Value.Age)
+            .TryGetFirst(x => x.Key.StartsWith("vfx/lockon/eff/m0361trg_a"), out var effect))
+            _myNumber = int.Parse(effect.Key.Replace("vfx/lockon/eff/m0361trg_a", "")[0].ToString());
+    }
+
+    public override void OnObjectEffect(uint target, ushort data1, ushort data2)
+    {
+        var targetObject = target.GetObject();
+        // PluginLog.Warning($"Name:{targetObject.Name} DataID: {targetObject.DataId} Data1: {data1}, Data2: {data2}");
+        if (WormholeDataIds.All(x => x != targetObject?.DataId)) return;
+        if (data1 == 4 && data2 == 8 && _wormholeChangedCount > 5)
+        {
+            _isStartWormholeFormation = false;
+            return;
+        }
+
+        if (data1 != 1 && data2 != 2) return;
+        var wormholePosition = targetObject.Position.ToVector2();
+        if (wormholePosition is { X: > 100, Y: < 100 } or { X: < 100, Y: > 100 }) _shouldInvert = true;
+        _wormholeChangedCount++;
+        if (_wormholeChangedCount is 3 or 5 or 7) _currentPhase++;
+    }
+
+    public override void OnReset()
+    {
+        _isStartWormholeFormation = false;
+        _myNumber = 0;
+        _shouldInvert = false;
+        _currentPhase = 0;
+        _wormholeChangedCount = 0;
+        _chakramScheduler?.Dispose();
+        _chakramScheduler = null;
+    }
+
+    public override void OnUpdate()
+    {
+        if (!_isStartWormholeFormation)
+        {
+            Controller.GetRegisteredElements().Each(x => x.Value.Enabled = false);
+            return;
+        }
+
+        for (var i = 1; i <= 8; i++)
+            if (Controller.TryGetElementByName($"Bait{i}", out var element))
+            {
+                var position = _baitPositions[i][_currentPhase];
+                var number = i;
+                if (_shouldInvert && _invertApplyIndex[_currentPhase].Contains(i))
+                {
+                    position = position with { X = 200 - position.X };
+                    number = number % 2 == 0 ? number - 1 : number + 1;
+                }
+
+                element.SetOffPosition(position.ToVector3());
+                element.Enabled = true;
+                if (number == _myNumber)
+                {
+                    element.overlayText = C.BaitText;
+                    element.color = GradientColor.Get(C.BaitColor1, C.BaitColor2).ToUint();
+                    element.tether = true;
+                    element.thicc = 10f;
+                }
+                else if (C.ShouldDisplayOtherBait)
+                {
+                    element.overlayText = number.ToString();
+                    element.color = C.OtherBaitColor.ToUint();
+                    element.tether = false;
+                    element.thicc = 2f;
+                }
+                else
+                {
+                    element.Enabled = false;
+                }
+            }
+    }
+
+    public override void OnSettingsDraw()
+    {
+        if (ImGui.CollapsingHeader("My Bait Settings:"))
+        {
+            ImGui.Indent();
+            ImGui.Text("Bait Text:");
+            ImGui.InputText("", ref C.BaitText, 100);
+            ImGui.Text("Bait Color:");
+            ImGuiComponents.HelpMarker(
+                "Change the color of the bait and the text that will be displayed on your bait.\nSetting different values makes it rainbow.");
+            ImGui.Indent();
+            ImGui.ColorEdit4("Color 1", ref C.BaitColor1, ImGuiColorEditFlags.NoInputs);
+            ImGui.SameLine();
+            ImGui.ColorEdit4("Color 2", ref C.BaitColor2, ImGuiColorEditFlags.NoInputs);
+            ImGui.Unindent();
+            ImGui.Unindent();
+        }
+
+        if (ImGui.CollapsingHeader("Other Bait Settings:"))
+        {
+            ImGui.Indent();
+            ImGui.Checkbox("Display Other Bait", ref C.ShouldDisplayOtherBait);
+            ImGui.Text("Other Bait Color:");
+            ImGuiComponents.HelpMarker("Change the color of the bait that will be displayed on other bait.");
+            ImGui.Indent();
+            ImGui.ColorEdit4("Color", ref C.OtherBaitColor, ImGuiColorEditFlags.NoInputs);
+            ImGui.Unindent();
+            ImGui.Unindent();
+        }
+    }
+
+    public class Config : IEzConfig
+    {
+        public Vector4 BaitColor1 = 0xFFFF00FF.ToVector4();
+        public Vector4 BaitColor2 = 0xFFFFFF00.ToVector4();
+        public string BaitText = "Go Here";
+        public Vector4 OtherBaitColor = 0xFFFF0000.ToVector4();
+        public bool ShouldDisplayOtherBait = true;
+    }
+}
---
+++ SplatoonScripts/Duties/Shadowbringers/TEA P4 Fate Projection β.cs
@@ -0,0 +1,257 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using ECommons;
+using ECommons.GameHelpers;
+using ECommons.Hooks.ActionEffectTypes;
+using ECommons.ImGuiMethods;
+using ECommons.Logging;
+using Splatoon;
+using Splatoon.SplatoonScripting;
+
+namespace SplatoonScriptsOfficial.Duties.Shadowbringers;
+
+public class TEA_P4_Fate_Projection_β : SplatoonScript
+{
+    public enum FutureActionType : byte
+    {
+        EastUpperLeft,
+        EastCenterLeft,
+        EastLowerLeft,
+        EastCenter,
+        North,
+        Spread,
+        Stack,
+        None
+    }
+
+    private readonly List<uint> _futurePlayers = [];
+
+    private bool _canAddFuturePlayer = true;
+
+    private bool _isStartFateProjectionCasting;
+
+    private bool _myDefuffIsYellow;
+    private uint? _myFuturePlayer;
+
+    public override Metadata? Metadata => new(1, "Garume");
+    public override HashSet<uint>? ValidTerritories => [887];
+
+    public override void OnStartingCast(uint source, uint castId)
+    {
+        if (castId == 19219)
+        {
+            _isStartFateProjectionCasting = true;
+            PluginLog.Warning("Start Fate Projection Casting");
+
+            Controller.Schedule(() =>
+            {
+                if (Controller.TryGetElementByName("FirstBait", out var firstElement))
+                    firstElement.Enabled = false;
+                if (Controller.TryGetElementByName("FirstText", out var firstTextElement))
+                    firstTextElement.Enabled = false;
+                if (Controller.TryGetElementByName("SecondText", out var secondElement))
+                    secondElement.overlayTextColor = EColor.Red.ToUint();
+            }, 1000 * 55);
+            Controller.Schedule(() =>
+            {
+                if (Controller.TryGetElementByName("SecondText", out var secondTextElement))
+                {
+                    secondTextElement.overlayTextColor = EColor.White.ToUint();
+                    secondTextElement.Enabled = false;
+                }
+
+                if (Controller.TryGetElementByName("SecondBait", out var secondElement))
+                {
+                    secondElement.Enabled = true;
+                    secondElement.tether = true;
+                }
+            }, 1000 * 63);
+            Controller.Schedule(() =>
+            {
+                _isStartFateProjectionCasting = false;
+                if (Controller.TryGetElementByName("SecondBait", out var secondElement)) secondElement.tether = false;
+            }, 1000 * 70);
+        }
+    }
+
+    public override void OnReset()
+    {
+        _isStartFateProjectionCasting = false;
+        _futurePlayers.Clear();
+        _myFuturePlayer = null;
+        _myDefuffIsYellow = false;
+        _canAddFuturePlayer = true;
+        Controller.GetRegisteredElements().Each(x => x.Value.Enabled = false);
+    }
+
+    public override void OnSetup()
+    {
+        var element = new Element(0)
+        {
+            color = EColor.Blue.ToUint(),
+            thicc = 5f
+        };
+
+        Controller.RegisterElement("FirstBait", element, true);
+
+        var secondElement = new Element(0)
+        {
+            radius = 1f,
+            color = EColor.Blue.ToUint(),
+            overlayText = "足元へ！",
+            overlayFScale = 2f,
+            overlayVOffset = 2f,
+            thicc = 5f
+        };
+
+        Controller.RegisterElement("SecondBait", secondElement, true);
+
+        var firstTextElement = new Element(0)
+        {
+            overlayText = "",
+            overlayVOffset = 8f,
+            overlayFScale = 5f,
+            Filled = false,
+            radius = 0f
+        };
+        firstTextElement.SetOffPosition(new Vector3(100f, 0, 100f));
+        Controller.RegisterElement("FirstText", firstTextElement, true);
+
+        var secondTextElement = new Element(0)
+        {
+            overlayText = "",
+            overlayVOffset = 5f,
+            overlayFScale = 5f,
+            Filled = false,
+            radius = 0f
+        };
+        secondTextElement.SetOffPosition(new Vector3(100f, 0, 100f));
+        Controller.RegisterElement("SecondText", secondTextElement, true);
+    }
+
+    public override void OnUpdate()
+    {
+        if (!_isStartFateProjectionCasting) Controller.GetRegisteredElements().Each(x => x.Value.Enabled = false);
+    }
+
+    private string GetFutureActionText(FutureActionType type)
+    {
+        var stackDirection = _myDefuffIsYellow ? "北に" : "南に";
+        return type switch
+        {
+            FutureActionType.EastCenter => "終了後、外周に行け",
+            FutureActionType.EastCenterLeft => "終了後、外周に行け",
+            FutureActionType.EastLowerLeft => "終了後、外周に行け",
+            FutureActionType.EastUpperLeft => "終了後、外周に行け",
+            FutureActionType.Spread => "散開",
+            FutureActionType.Stack => $"頭割り {stackDirection}",
+            _ => ""
+        };
+    }
+
+    private Vector2 GetFutureActionPosition(FutureActionType type)
+    {
+        return type switch
+        {
+            FutureActionType.EastUpperLeft => new Vector2(112f, 98.5f),
+            FutureActionType.EastCenterLeft => new Vector2(112f, 100f),
+            FutureActionType.EastLowerLeft => new Vector2(112f, 101.5f),
+            FutureActionType.EastCenter => new Vector2(115, 100),
+            FutureActionType.North => new Vector2(92, 84f),
+            FutureActionType.None => Vector2.Zero,
+            _ => Vector2.Zero
+        };
+    }
+
+    public override void OnActionEffectEvent(ActionEffectSet set)
+    {
+        if (!_isStartFateProjectionCasting) return;
+        if (set.Source is not { DataId: 0x2C55 }) return;
+        switch (set.Action)
+        {
+            case { RowId: 18592 }:
+            {
+                var text = GetFutureActionText(FutureActionType.Spread);
+                if (Controller.TryGetElementByName("SecondText", out var textElement))
+                {
+                    textElement.overlayText = text;
+                    textElement.Enabled = true;
+                }
+
+                break;
+            }
+            case { RowId: 18593 }:
+            {
+                var text = GetFutureActionText(FutureActionType.Stack);
+                if (Controller.TryGetElementByName("SecondText", out var textElement))
+                {
+                    textElement.overlayText = text;
+                    textElement.Enabled = true;
+                }
+
+                break;
+            }
+        }
+
+        if (set.Action is { RowId: 18590 })
+            if (Controller.TryGetElementByName("SecondBait", out var element))
+            {
+                element.SetOffPosition(set.Source.Position);
+                element.Enabled = true;
+            }
+    }
+
+    public override void OnTetherCreate(uint source, uint target, uint data2, uint data3, uint data5)
+    {
+        if (!_isStartFateProjectionCasting) return;
+
+        if (_canAddFuturePlayer) _futurePlayers.Add(target);
+
+
+        if (source == Player.Object.EntityId)
+        {
+            _myFuturePlayer = target;
+            Controller.Schedule(() =>
+            {
+                _canAddFuturePlayer = false;
+                var reversed = _futurePlayers.ToArray().Reverse().ToArray();
+                var futureAction = FutureActionType.None;
+                for (var i = 0; i < reversed.Length; i++)
+                    if (_myFuturePlayer == reversed[i])
+                    {
+                        futureAction = i switch
+                        {
+                            0 => FutureActionType.EastCenter,
+                            1 => FutureActionType.North,
+                            2 => FutureActionType.EastCenterLeft,
+                            3 => FutureActionType.EastUpperLeft,
+                            4 => FutureActionType.EastUpperLeft,
+                            5 => FutureActionType.EastUpperLeft,
+                            6 => FutureActionType.EastLowerLeft,
+                            7 => FutureActionType.EastUpperLeft,
+                            _ => FutureActionType.None
+                        };
+
+                        if (i is 1 or 3 or 5 or 7)
+                            _myDefuffIsYellow = true;
+                    }
+
+                if (Controller.TryGetElementByName("FirstBait", out var element))
+                {
+                    var position = GetFutureActionPosition(futureAction);
+                    element.SetOffPosition(new Vector3(position.X, 0, position.Y));
+                    element.tether = true;
+                    element.Enabled = true;
+                }
+
+                if (Controller.TryGetElementByName("FirstText", out var textElement))
+                {
+                    var text = GetFutureActionText(futureAction);
+                    textElement.overlayText = text;
+                    textElement.Enabled = true;
+                }
+            }, 2000);
+        }
+    }
+}

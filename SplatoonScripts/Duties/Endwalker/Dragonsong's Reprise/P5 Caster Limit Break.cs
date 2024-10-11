cfa4f1cd252a765e21ffb90121e8d83699e6601b
ガルム <desuboruto@gmail.com>
2024-10-12 06:20:32 +0900

    [improve] caster limit break script

--- SplatoonScripts/Duties/Endwalker/Dragonsong's Reprise/P5 Caster Limit Break.cs
+++ SplatoonScripts/Duties/Endwalker/Dragonsong's Reprise/P5 Caster Limit Break.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Numerics;
 using Dalamud.Game.Command;
@@ -14,10 +15,24 @@ namespace SplatoonScriptsOfficial.Duties.Endwalker.Dragonsong_s_Reprise;
 
 public class P5_Caster_Limit_Break : SplatoonScript
 {
+    public enum Direction : byte
+    {
+        NorthNorthWest,
+        WestNorthWest,
+        WestSouthWest,
+        SouthSouthWest,
+        SouthSouthEast,
+        EastSouthEast,
+        EastNorthEast,
+        NorthNorthEast
+    }
+
     private const uint LimitBreak2Id = 204;
     public override HashSet<uint>? ValidTerritories => [];
 
-    public Config C => Controller.GetConfig<Config>();
+    private Config C => Controller.GetConfig<Config>();
+
+    public override Metadata? Metadata => new(2, "Garume");
 
     public override void OnEnable()
     {
@@ -29,11 +44,12 @@ public class P5_Caster_Limit_Break : SplatoonScript
         Svc.Commands.RemoveHandler("/limitBreak");
     }
 
-    public void OnCastLimitBreak(string command, string arguments)
+    private void OnCastLimitBreak(string command, string arguments)
     {
-        var result = UseLimitBreak();
+        var targetPosition = TargetPosition(C.Direction);
+        var result = UseLimitBreak(targetPosition);
         if (result)
-            DuoLog.Debug($"Limit Break casted to {C.TargetPosition}");
+            DuoLog.Debug($"Limit Break casted to {targetPosition}");
         else
             DuoLog.Warning("Failed to cast Limit Break");
     }
@@ -41,20 +57,46 @@ public class P5_Caster_Limit_Break : SplatoonScript
     public override void OnSettingsDraw()
     {
         ImGui.Text("Limit Break Settings");
-        ImGui.InputFloat3("Target Position", ref C.TargetPosition);
+        ImGuiEx.EnumCombo("Direction", ref C.Direction);
 
-        if (ImGuiEx.CollapsingHeader("Debug")) ImGui.Text($"My Position: {Player.Position}");
+        if (ImGuiEx.CollapsingHeader("Debug"))
+        {
+            ImGui.Text($"My Position: {Player.Position}");
+            ImGui.Text($"Target Position: {TargetPosition(C.Direction)}");
+        }
     }
 
-    public unsafe bool UseLimitBreak()
+    private unsafe bool UseLimitBreak(Vector3 targetPosition)
     {
-        var targetPosition = C.TargetPosition;
-        var targetId = 0xE0000000;
+        const uint targetId = 0xE0000000;
         return ActionManager.Instance()->UseActionLocation(ActionType.Action, LimitBreak2Id, targetId, &targetPosition);
+    }
+
+    private Vector3 TargetPosition(Direction direction)
+    {
+        const float radius = 5.27f; 
+        Vector2 center = new(100, 100);
+        const float angleOffset = 22f;
+        var angle = direction switch
+        {
+            Direction.NorthNorthWest => 90f + angleOffset,
+            Direction.WestNorthWest => 180f - angleOffset,
+            Direction.WestSouthWest => 180f + angleOffset,
+            Direction.SouthSouthWest => 270f - angleOffset,
+            Direction.SouthSouthEast => 270f + angleOffset,
+            Direction.EastSouthEast => 360f - angleOffset,
+            Direction.EastNorthEast => 0f + angleOffset,
+            Direction.NorthNorthEast => 90f - angleOffset,
+            _ => throw new ArgumentOutOfRangeException(nameof(direction), direction, null)
+        };
+        
+        var x = center.X + radius * MathF.Cos(MathF.PI * angle / 180);
+        var y = center.Y - radius * MathF.Sin(MathF.PI * angle / 180);
+        return new Vector3(x,0, y);
     }
 
     public class Config : IEzConfig
     {
-        public Vector3 TargetPosition = new(0, 0, 0);
+        public Direction Direction = Direction.NorthNorthWest;
     }
 }

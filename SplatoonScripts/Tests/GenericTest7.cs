0b042f2336971222b200697f3f7de2cdbb464e78
Limiana <5073202+Limiana@users.noreply.github.com>
2024-11-20 09:24:58 +0300

    Reduce logging

--- SplatoonScripts/Tests/GenericTest7.cs
+++ SplatoonScripts/Tests/GenericTest7.cs
@@ -8,7 +8,9 @@ using ECommons.Throttlers;
 using FFXIVClientStructs.FFXIV.Client.UI;
 using FFXIVClientStructs.FFXIV.Client.UI.Agent;
 using FFXIVClientStructs.FFXIV.Client.UI.Misc;
+using FFXIVClientStructs.FFXIV.Common.Component.Excel;
 using FFXIVClientStructs.FFXIV.Component.GUI;
+using ImGuiNET;
 using Splatoon.SplatoonScripting;
 using System;
 using System.Buffers;
@@ -20,28 +22,4 @@ public unsafe class GenericTest7 : SplatoonScript
     public override HashSet<uint>? ValidTerritories { get; }
 
     public override Metadata? Metadata => new(5);
-
-    
-
-    public override void OnEnable()
-    {
-        EzSignatureHelper.Initialize(this);
-    }
-
-    public override void OnUpdate()
-    {
-        var trade = AgentModule.Instance()->GetAgentByInternalId(AgentId.Trade);
-        if(trade->IsAgentActive() && GenericHelpers.TryGetAddonByName<AtkUnitBase>("Trade", out var addon))
-        {
-            if(FrameThrottler.Throttle("AntiTrade", 2))
-            {
-                Callback.Fire(addon, true, -1);
-                DuoLog.Information("Prevent trade");
-            }
-        }
-    }
-
-    public override void OnSettingsDraw()
-    {
-    }
 }

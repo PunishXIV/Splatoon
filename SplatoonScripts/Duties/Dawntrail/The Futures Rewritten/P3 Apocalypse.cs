6acd9f64e5e1e5925ba7c8c2191e58aa6e5e991b
Limiana <5073202+Limiana@users.noreply.github.com>
2024-12-08 17:42:47 +0300

    Update apoc script

--- SplatoonScripts/Duties/Dawntrail/The Futures Rewritten/P3 Apocalypse.cs
+++ SplatoonScripts/Duties/Dawntrail/The Futures Rewritten/P3 Apocalypse.cs
@@ -1,6 +1,9 @@
 ﻿using ECommons;
+using ECommons.Configuration;
 using ECommons.DalamudServices;
+using ECommons.ImGuiMethods;
 using ECommons.MathHelpers;
+using ImGuiNET;
 using Splatoon.SplatoonScripting;
 using Splatoon.Utility;
 using System;
@@ -13,7 +16,7 @@ namespace SplatoonScriptsOfficial.Duties.Dawntrail.The_Futures_Rewritten;
 public class P3_Apocalypse : SplatoonScript
 {
     public override HashSet<uint>? ValidTerritories => [1238];
-    public override Metadata? Metadata => new(2, "Errer, NightmareXIV");
+    public override Metadata? Metadata => new(3, "Errer, NightmareXIV");
     public long StartTime = 0;
 
     Dictionary<int, Vector2> Positions = new()
@@ -36,6 +39,10 @@ public class P3_Apocalypse : SplatoonScript
             Controller.RegisterElementFromCode($"Circle{i}", "{\"Name\":\"Circle\",\"Enabled\":false,\"refX\":100.0,\"refY\":100.0,\"radius\":9.0,\"refActorTetherTimeMin\":0.0,\"refActorTetherTimeMax\":0.0}");
             Controller.RegisterElementFromCode($"EarlyCircle{i}", "{\"Name\":\"Circle\",\"Enabled\":false,\"refX\":100.0,\"refY\":100.0,\"radius\":9.0,\"color\":3355508223,\"fillIntensity\":0.25,\"refActorTetherTimeMin\":0.0,\"refActorTetherTimeMax\":0.0}");
         }
+        Controller.RegisterElementFromCode("TankLine", "{\"Name\":\"\",\"type\":2,\"Enabled\":false,\"radius\":0.0,\"color\":3372209152,\"fillIntensity\":0.345,\"thicc\":4.0,\"refActorTetherTimeMin\":0.0,\"refActorTetherTimeMax\":0.0}");
+        Controller.RegisterElementFromCode("Line2", "{\"Name\":\"\",\"type\":2,\"Enabled\":false,\"radius\":0.0,\"fillIntensity\":0.345,\"thicc\":4.0,\"refActorTetherTimeMin\":0.0,\"refActorTetherTimeMax\":0.0}");
+        Controller.RegisterElementFromCode("LineRot1", "{\"Name\":\"\",\"type\":2,\"Enabled\":false,\"radius\":0.0,\"color\":3355508484,\"fillIntensity\":0.345,\"thicc\":4.0,\"LineEndA\":1,\"refActorTetherTimeMin\":0.0,\"refActorTetherTimeMax\":0.0}");
+        Controller.RegisterElementFromCode("LineRot2", "{\"Name\":\"\",\"type\":2,\"Enabled\":false,\"radius\":0.0,\"color\":3355508484,\"fillIntensity\":0.345,\"thicc\":4.0,\"LineEndA\":1,\"refActorTetherTimeMin\":0.0,\"refActorTetherTimeMax\":0.0}");
     }
 
     int[][] Clockwise = [[0, 1, -1], [0, 1, -1, 2, -2], [1, 2, 3, -1, -2, -3], [2, 3, 4, -2, -3, -4], [1, 3, 4, -1, -3, -4], [1,2,4,-1,-2,-4]];
@@ -79,8 +86,54 @@ public class P3_Apocalypse : SplatoonScript
             var isClockwise = far.Rotation.RadToDeg().InRange(45,45+90);
             var set = isClockwise ? Clockwise : CounterClockwise;
             var phase = Environment.TickCount64 - StartTime;
-            if(phase > 6000)
+            if(C.ShowInitialApocMove && phase < 15000)
             {
+                if(Controller.TryGetElementByName("Line2", out var line))
+                {
+                    line.Enabled = true;
+                    var linePos1 = Positions[1].ToVector3(0);
+                    var linePos2 = Positions[-1].ToVector3(0);
+                    line.SetRefPosition(MathHelper.RotateWorldPoint(new(100, 0, 100), angle.DegreesToRadians(), linePos1));
+                    line.SetOffPosition(MathHelper.RotateWorldPoint(new(100, 0, 100), angle.DegreesToRadians(), linePos2));
+                }
+            }
+            if(C.ShowMoveGuide && phase < C.TankDelayMS)
+            {
+                {
+                    if(Controller.TryGetElementByName("LineRot1", out var line))
+                    {
+                        line.Enabled = true;
+                        var linePos1 = Positions[isClockwise ? -4 : 2].ToVector3(0);
+                        var linePos2 = Positions[1].ToVector3(0);
+                        line.SetRefPosition(MathHelper.RotateWorldPoint(new(100, 0, 100), angle.DegreesToRadians(), linePos1));
+                        line.SetOffPosition(MathHelper.RotateWorldPoint(new(100, 0, 100), angle.DegreesToRadians(), linePos2));
+                    }
+                }
+                {
+                    if(Controller.TryGetElementByName("LineRot2", out var line))
+                    {
+                        line.Enabled = true;
+                        var linePos1 = Positions[isClockwise ? 4 : -2].ToVector3(0);
+                        var linePos2 = Positions[-1].ToVector3(0);
+                        line.SetRefPosition(MathHelper.RotateWorldPoint(new(100, 0, 100), angle.DegreesToRadians(), linePos1));
+                        line.SetOffPosition(MathHelper.RotateWorldPoint(new(100, 0, 100), angle.DegreesToRadians(), linePos2));
+                    }
+                }
+            }
+            if(phase > C.DelayMS)
+            {
+                /*{
+                    if(Controller.TryGetElementByName("Line", out var line))
+                    {
+                        line.Enabled = true;
+                        var linePos1 = Positions[isClockwise ? 4 : 2].ToVector3(0);
+                        var linePos2 = Positions[isClockwise ? -4 : -2].ToVector3(0);
+                        line.SetRefPosition(MathHelper.RotateWorldPoint(new(100, 0, 100), angle.DegreesToRadians(), linePos1));
+                        line.SetOffPosition(MathHelper.RotateWorldPoint(new(100, 0, 100), angle.DegreesToRadians(), linePos2));
+                    }
+                }*/
+                
+
                 for(int i = 0; i < 6; i++)
                 {
                     if(phase < InitialDelay + Delay * i)
@@ -94,6 +147,38 @@ public class P3_Apocalypse : SplatoonScript
                     }
                 }
             }
+            if(C.ShowTankGuide && phase > C.TankDelayMS)
+            {
+                if(Controller.TryGetElementByName("TankLine", out var line))
+                {
+                    line.Enabled = true;
+                    var linePos1 = Positions[3].ToVector3(0);
+                    var linePos2 = Positions[-3].ToVector3(0);
+                    line.SetRefPosition(MathHelper.RotateWorldPoint(new(100, 0, 100), angle.DegreesToRadians(), linePos1));
+                    line.SetOffPosition(MathHelper.RotateWorldPoint(new(100, 0, 100), angle.DegreesToRadians(), linePos2));
+                }
+            }
         }
+    }
+
+    public override void OnSettingsDraw()
+    {
+        ImGui.SetNextItemWidth(200f);
+        ImGuiEx.SliderIntAsFloat("Delay before displaying AOE", ref C.DelayMS, 0, 12000);
+        ImGui.Checkbox("Show initial movement", ref C.ShowInitialApocMove);
+        ImGui.Checkbox("Show move guide for party (arrows to safe spot from initial movement)", ref C.ShowMoveGuide);
+        ImGui.Checkbox("Show tank bait guide (beta)", ref C.ShowTankGuide);
+        ImGui.SetNextItemWidth(200f);
+        ImGuiEx.SliderIntAsFloat("Hide move guide and switch to tank bait guide at", ref C.TankDelayMS, 0, 30000);
+    }
+
+    Config C => Controller.GetConfig<Config>();
+    public class Config : IEzConfig
+    {
+        public int DelayMS = 6000;
+        public bool ShowInitialApocMove = true;
+        public bool ShowMoveGuide = false;
+        public bool ShowTankGuide = false;
+        public int TankDelayMS = 18000;
     }
 }
---
+++ SplatoonScripts/Generic/AutoDutyEnableTankStance.cs
@@ -0,0 +1,38 @@
+﻿using Dalamud.Game.ClientState.Conditions;
+using ECommons;
+using ECommons.Automation;
+using ECommons.DalamudServices;
+using ECommons.ExcelServices;
+using ECommons.EzIpcManager;
+using ECommons.GameHelpers;
+using ECommons.Throttlers;
+using Splatoon.SplatoonScripting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SplatoonScriptsOfficial.Generic;
+public class AutoDutyEnableTankStance : SplatoonScript
+{
+    public override HashSet<uint>? ValidTerritories { get; } = [];
+    [EzIPC] public Func<bool> IsStopped;
+
+    public override void OnSetup()
+    {
+        EzIPC.Init(this, "AutoDuty", SafeWrapper.AnyException);
+    }
+
+    public override void OnUpdate()
+    {
+        if(Svc.Condition[ConditionFlag.BoundByDuty56] && !Player.IsAnimationLocked && EzThrottler.Throttle($"{nameof(AutoDutyEnableTankStance)}_EnableTS", 5000))
+        {
+            if(!IsStopped() && Player.Interactable && !Player.Object.StatusList.Any(x => x.StatusId.EqualsAny<uint>(1833)))
+            {
+                if(Player.Job == Job.GNB)
+                {
+                    Chat.Instance.ExecuteAction(16142);
+                }
+            }
+        }
+    }
+}

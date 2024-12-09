451c210aa6e4e585b0f7153da19e46fe6a0b09f1
Limiana <5073202+Limiana@users.noreply.github.com>
2024-12-09 09:21:21 +0300

    Player role list 1

---
+++ Splatoon/SplatoonScripting/Priority/RolePlayerAssignment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Splatoon.SplatoonScripting.Priority;
+public class RolePlayerAssignment
+{
+    public uint Territory;
+    public List<JobbedPlayer> Players = [];
+    public List<RolePosition> RolePositions = [];
+}

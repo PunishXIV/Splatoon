8fe82f627f7abd75e20c7cf16725ac39cd725395
ガルム <desuboruto@gmail.com>
2024-12-18 21:30:49 +0900

    [fix] version up

--- SplatoonScripts/Duties/Dawntrail/The Futures Rewritten/P4 Crystallize Time Refined.cs
+++ SplatoonScripts/Duties/Dawntrail/The Futures Rewritten/P4 Crystallize Time Refined.cs
@@ -86,7 +86,7 @@ public unsafe class P4_Crystallize_Time_Refined : SplatoonScript
     private bool IsActive => Svc.Objects.Any(x => x.DataId == 17837) && !BasePlayer.IsDead;
 
     public override HashSet<uint>? ValidTerritories => [1238];
-    public override Metadata? Metadata => new(6, "Garume, NightmareXIV");
+    public override Metadata? Metadata => new(7, "Garume, NightmareXIV");
 
     private Config C => Controller.GetConfig<Config>();
 

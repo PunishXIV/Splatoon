a06e094fefca8a769f3ea0c6005bdff1a8541510
Limiana <5073202+Limiana@users.noreply.github.com>
2024-12-21 05:12:54 +0300

    Update

---
+++ SplatoonScripts/Duties/Shadowbringers/The Epic Of Alexander/TEA P1 Untarget Doll.cs
@@ -0,0 +1,21 @@
+﻿using Dalamud.Game.ClientState.Objects.Types;
+using ECommons;
+using ECommons.DalamudServices;
+using ECommons.ExcelServices.TerritoryEnumeration;
+using Splatoon.SplatoonScripting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SplatoonScriptsOfficial.Duties.Shadowbringers.The_Epic_Of_Alexander;
+public class TEA_P1_Untarget_Doll : SplatoonScript
+{
+    public override HashSet<uint>? ValidTerritories { get; } = [Raids.The_Epic_of_Alexander_Ultimate];
+
+    public override void OnUpdate()
+    {
+        if(Svc.Targets.Target is IBattleNpc b && b.NameId.EqualsAny<uint>(3759, 9214) && ((float)b.CurrentHp / (float)b.MaxHp) < 0.24f)
+        {
+            Svc.Targets.Target = Svc.Objects.OfType<IBattleNpc>().FirstOrDefault(x => x.IsTargetable && !x.IsDead && x.NameId.EqualsAny<uint>(3765, 9211, 9212));
+        }
+    }
+}

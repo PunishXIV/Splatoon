6e69440ea6d7a8206bee8c40fc4f1d18854c4d03
ガルム <desuboruto@gmail.com>
2025-02-14 21:02:06 +0900

    [support] new priority

--- SplatoonScripts/Duties/Endwalker/Dragonsong's Reprise/P5 Death of the Heavens.cs
+++ SplatoonScripts/Duties/Endwalker/Dragonsong's Reprise/P5 Death of the Heavens.cs
@@ -12,7 +12,6 @@ using ECommons.ChatMethods;
 using ECommons.Configuration;
 using ECommons.DalamudServices;
 using ECommons.DalamudServices.Legacy;
-using ECommons.ExcelServices;
 using ECommons.GameFunctions;
 using ECommons.GameHelpers;
 using ECommons.Hooks;
@@ -20,12 +19,11 @@ using ECommons.Hooks.ActionEffectTypes;
 using ECommons.ImGuiMethods;
 using ECommons.Logging;
 using ECommons.MathHelpers;
-using ECommons.PartyFunctions;
 using FFXIVClientStructs.FFXIV.Client.Game;
-using FFXIVClientStructs.FFXIV.Client.UI.Info;
 using ImGuiNET;
 using Splatoon;
 using Splatoon.SplatoonScripting;
+using Splatoon.SplatoonScripting.Priority;
 
 namespace SplatoonScriptsOfficial.Duties.Endwalker.Dragonsong_s_Reprise;
 
@@ -48,11 +46,10 @@ public unsafe class P5_Death_of_the_Heavens : SplatoonScript
     private Vector3 _lastPlayerPosition = Vector3.Zero;
     private BaitType _myBait = BaitType.None;
     private PlaystationMarker _myMarker = PlaystationMarker.Circle;
-    private readonly ImGuiEx.RealtimeDragDrop<Job> DragDrop = new("DragDropJob", x => x.ToString());
 
     public override HashSet<uint>? ValidTerritories => [968];
     private Config C => Controller.GetConfig<Config>();
-    public override Metadata? Metadata => new(5, "Garume");
+    public override Metadata? Metadata => new(6, "Garume");
 
     private IBattleChara? Thordan => Svc.Objects.OfType<IBattleChara>()
         .FirstOrDefault(x => x.NameId == 0xE30 && x.IsCharacterVisible());
@@ -117,70 +114,6 @@ public unsafe class P5_Death_of_the_Heavens : SplatoonScript
         return position;
     }
 
-    private bool DrawPriorityList()
-    {
-        if (C.Priority.Length != 8)
-            C.Priority = ["", "", "", "", "", "", "", ""];
-
-        ImGuiEx.Text("Priority list");
-        ImGui.SameLine();
-        ImGuiEx.Spacing();
-        if (ImGui.Button("Perform test")) SelfTest();
-        ImGui.SameLine();
-        if (ImGui.Button("Fill by job"))
-        {
-            HashSet<(string, Job)> party = [];
-            foreach (var x in FakeParty.Get())
-                party.Add((x.Name.ToString(), x.GetJob()));
-
-            var proxy = InfoProxyCrossRealm.Instance();
-            for (var i = 0; i < proxy->GroupCount; i++)
-            {
-                var group = proxy->CrossRealmGroups[i];
-                for (var c = 0; c < proxy->CrossRealmGroups[i].GroupMemberCount; c++)
-                {
-                    var x = group.GroupMembers[c];
-                    party.Add((x.Name.Read(), (Job)x.ClassJobId));
-                }
-            }
-
-            var index = 0;
-            foreach (var job in C.Jobs.Where(job => party.Any(x => x.Item2 == job)))
-            {
-                C.Priority[index] = party.First(x => x.Item2 == job).Item1;
-                index++;
-            }
-
-            for (var i = index; i < C.Priority.Length; i++)
-                C.Priority[i] = "";
-        }
-        ImGuiEx.Tooltip("The list is populated based on the job.\nYou can adjust the priority from the option header.");
-
-        ImGui.PushID("prio");
-        for (var i = 0; i < C.Priority.Length; i++)
-        {
-            ImGui.PushID($"prioelement{i}");
-            ImGui.Text($"Character {i + 1}");
-            ImGui.SameLine();
-            ImGui.SetNextItemWidth(200);
-            ImGui.InputText($"##Character{i}", ref C.Priority[i], 50);
-            ImGui.SameLine();
-            ImGui.SetNextItemWidth(150);
-            if (ImGui.BeginCombo("##partysel", "Select from party"))
-            {
-                foreach (var x in FakeParty.Get().Select(x => x.Name.ToString())
-                             .Union(UniversalParty.Members.Select(x => x.Name)).ToHashSet())
-                    if (ImGui.Selectable(x))
-                        C.Priority[i] = x;
-                ImGui.EndCombo();
-            }
-
-            ImGui.PopID();
-        }
-
-        ImGui.PopID();
-        return false;
-    }
 
     public override void OnReset()
     {
@@ -193,7 +126,7 @@ public unsafe class P5_Death_of_the_Heavens : SplatoonScript
     {
         ImGui.Text("General");
         ImGui.Indent();
-        DrawPriorityList();
+        C.Priority.Draw();
         ImGui.Text("Pre Playstation Split");
         ImGuiEx.EnumCombo("##Pre Playstation Split", ref C.PrePlaystationSplit);
         ImGui.Text("Orientation Base");
@@ -223,28 +156,7 @@ public unsafe class P5_Death_of_the_Heavens : SplatoonScript
 
         ImGui.Unindent();
 
-        if (ImGuiEx.CollapsingHeader("Option"))
-        {
-            DragDrop.Begin();
-            foreach (var job in C.Jobs)
-            {
-                DragDrop.NextRow();
-                ImGui.Text(job.ToString());
-                ImGui.SameLine();
 
-                if (ThreadLoadImageHandler.TryGetIconTextureWrap((uint)job.GetIcon(), false, out var texture))
-                {
-                    ImGui.Image(texture.ImGuiHandle, new Vector2(24f));
-                    ImGui.SameLine();
-                }
-
-                ImGui.SameLine();
-                DragDrop.DrawButtonDummy(job, C.Jobs, C.Jobs.IndexOf(job));
-            }
-
-            DragDrop.End();
-        }
-        
         if (ImGui.CollapsingHeader("Debug"))
         {
             ImGui.Checkbox("Show Debug Message", ref C.ShowDebug);
@@ -257,32 +169,16 @@ public unsafe class P5_Death_of_the_Heavens : SplatoonScript
     private void SelfTest()
     {
         Print("= P5 Death of the Heavens self-test =", UIColor.LightBlue);
-        var party = FakeParty.Get().ToArray();
-        var isCorrect = C.Priority.All(x => !string.IsNullOrEmpty(x));
+        var isCorrect = C.Priority.GetFirstValidList() != null;
 
         if (!isCorrect)
         {
+            Print("!!! Test failed !!!", UIColor.Red);
             Print("Priority list is not filled correctly.", UIColor.Red);
             return;
         }
 
-        if (party.Length != 8)
-        {
-            isCorrect = false;
-            Print("Can only be tested in content.", UIColor.Red);
-        }
-
-        foreach (var player in party)
-            if (C.Priority.All(x => x != player.Name.ToString()))
-            {
-                isCorrect = false;
-                Print($"Player {player.Name} is not in the priority list.", UIColor.Red);
-            }
-
-        if (isCorrect)
-            Print("Test Success!", UIColor.Green);
-        else
-            Print("!!! Test failed !!!", UIColor.Red);
+        Print("Test Success!", UIColor.Green);
         return;
 
         void Print(string message, UIColor color)
@@ -527,9 +423,9 @@ public unsafe class P5_Death_of_the_Heavens : SplatoonScript
 
                     var red = 0;
                     var blue = 0;
-                    foreach (var player in C.Priority)
+                    foreach (var player in C.Priority.GetFirstValidList()?.List)
                     {
-                        var p = players.FirstOrDefault(x => x.Name.ToString() == player);
+                        var p = players.FirstOrDefault(x => x.Name.ToString() == player.Name);
                         if (p == null)
                         {
                             DuoLog.Error($"Player {player} not found in party.");
@@ -630,36 +526,12 @@ public unsafe class P5_Death_of_the_Heavens : SplatoonScript
         public readonly Vector4 BaitColor1 = 0xFFFF00FF.ToVector4();
         public readonly Vector4 BaitColor2 = 0xFFFFFF00.ToVector4();
 
-        public readonly List<Job> Jobs =
-        [
-            Job.PLD,
-            Job.WAR,
-            Job.DRK,
-            Job.GNB,
-            Job.WHM,
-            Job.SCH,
-            Job.AST,
-            Job.SGE,
-            Job.VPR,
-            Job.DRG,
-            Job.MNK,
-            Job.SAM,
-            Job.RPR,
-            Job.NIN,
-            Job.BRD,
-            Job.MCH,
-            Job.DNC,
-            Job.BLM,
-            Job.SMN,
-            Job.RDM,
-            Job.PCT
-        ];
+        public readonly PriorityData Priority = new();
 
         public bool LockFace = true;
         public bool LockFaceEnableWhenNotMoving = true;
         public Direction OrientationBase = Direction.North;
         public PrePlaystationSplit PrePlaystationSplit = PrePlaystationSplit.Horizontal;
-        public string[] Priority = ["", "", "", "", "", "", "", ""];
         public bool ShouldCheckOnStart = true;
         public bool ShowDebug;
     }

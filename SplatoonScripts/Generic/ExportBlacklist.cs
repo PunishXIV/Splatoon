1ef9a34731951f8ddc42ae7fcdea9e77ca91f976
Limiana <5073202+Limiana@users.noreply.github.com>
2025-03-26 05:48:12 +0300

    Commit all changes

---
+++ SplatoonScripts/Generic/ExportBlacklist.cs
@@ -0,0 +1,38 @@
+﻿using Dalamud.Memory;
+using ECommons;
+using FFXIVClientStructs.FFXIV.Client.UI.Info;
+using ImGuiNET;
+using Splatoon.SplatoonScripting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SplatoonScriptsOfficial.Generic;
+public unsafe class ExportBlacklist : SplatoonScript
+{
+    public override HashSet<uint>? ValidTerritories { get; } = [9999];
+
+    public override void OnSettingsDraw()
+    {
+        if(ImGui.Button("Export blacklist"))
+        {
+            var s = "";
+            foreach(var x in InfoProxyBlacklist.Instance()->BlockedCharacters)
+            {
+                s += $"{BlockedCharaToString(x)}\n==========================\n";
+            }
+            GenericHelpers.Copy(s);
+        }
+    }
+
+    string BlockedCharaToString(InfoProxyBlacklist.BlockedCharacter c)
+    {
+        return $"""
+            Name: {MemoryHelper.ReadStringNullTerminated((nint)c.Name)},
+            ID: {c.Id}
+            Flag: {c.Flag}
+            """;
+    }
+}

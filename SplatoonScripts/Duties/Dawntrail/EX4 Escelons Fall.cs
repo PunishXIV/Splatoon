00de2edfe22fe6ed6016cc0e5d8dca59c0e6ee25
Limiana <5073202+Limiana@users.noreply.github.com>
2025-03-28 21:26:04 +0300

    Update script

--- SplatoonScripts/Duties/Dawntrail/EX4 Escelons Fall.cs
+++ SplatoonScripts/Duties/Dawntrail/EX4 Escelons Fall.cs
@@ -1,40 +1,41 @@
 ﻿using Dalamud.Game.ClientState.Objects.SubKinds;
 using Dalamud.Game.ClientState.Objects.Types;
+using ECommons;
 using ECommons.Configuration;
 using ECommons.DalamudServices;
+using ECommons.ExcelServices;
 using ECommons.GameHelpers;
-using ECommons.Hooks.ActionEffectTypes;
 using ECommons.Hooks;
+using ECommons.Hooks.ActionEffectTypes;
 using ECommons.ImGuiMethods;
-using ECommons;
+using ECommons.Logging;
+using FFXIVClientStructs.FFXIV.Client.Game;
 using ImGuiNET;
 using Splatoon.SplatoonScripting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Numerics;
-using ECommons.Logging;
-using ECommons.CircularBuffers;
-using FFXIVClientStructs.FFXIV.Client.Game;
 
 namespace SplatoonScriptsOfficial.Duties.Dawntrail;
 public unsafe class EX4_Escelons_Fall : SplatoonScript
 {
     public override HashSet<uint>? ValidTerritories { get; } = [1271];
 
-    public override Metadata? Metadata => new(1, "NightmareXIV");
+    public override Metadata? Metadata => new(3, "NightmareXIV, Redmoonwow");
 
     uint StatusCloseFar = 2970;
     uint StatusParamClose = 758;
     uint StatusParamFar = 759;
     uint[] CastSwitcher = [43182, 43181];
     uint CastStandard = 43181;
+    uint RoseBloom3rd = 43541;
     uint NpcNameId = 13861;//Name NPC ID: 13861
     int NumSwitches = 0;
     long ForceResetAt = long.MaxValue;
     List<bool> SequenceIsClose = [];
+    bool AdjustPhase = false;
+    bool THShockTargeted = false;
 
     IBattleNpc? Zelenia => Svc.Objects.OfType<IBattleNpc>().FirstOrDefault(x => x.NameId == this.NpcNameId && x.IsTargetable);
 
@@ -57,10 +58,14 @@ public unsafe class EX4_Escelons_Fall : SplatoonScript
         ImGuiEx.RadioButtonBool("First##2", "Second##2", ref C.TakeFirstIfFar);
         if(ImGui.CollapsingHeader("Debug"))
         {
+            ImGuiEx.Text($"AdjustPhase: {AdjustPhase}");
+            ImGuiEx.Text($"THShockTargeted: {THShockTargeted}");
             ImGuiEx.Text($"SequenceIsClose: {SequenceIsClose.Print()}");
-            ImGuiEx.Text($"{Svc.Objects.OfType<IPlayerCharacter>().OrderBy(x => Vector3.Distance(x.Position, Zelenia.Position)).Print("\n")}");
             ImGuiEx.Text($"GetMyCloses: {GetMyCloses().Print()}");
             ImGuiEx.Text($"IsSelfClose: {IsSelfClose()}");
+            ImGuiEx.Text($"NumSwitches: {NumSwitches}");
+            ImGuiEx.Text($"ForceResetAt: {ForceResetAt}");
+            ImGuiEx.Text($"{Svc.Objects.OfType<IPlayerCharacter>().OrderBy(x => Vector3.Distance(x.Position, Zelenia.Position)).Print("\n")}");
         }
     }
 
@@ -77,6 +82,31 @@ public unsafe class EX4_Escelons_Fall : SplatoonScript
     List<bool> GetMyCloses()
     {
         var myCloseFirst = this.SequenceIsClose[0] ? C.TakeFirstIfClose : C.TakeFirstIfFar;
+        if(this.AdjustPhase)
+        {
+            if(THShockTargeted)
+            {
+                if(Player.Job is Job.DRK or Job.WAR or Job.GNB or Job.PLD or Job.WHM or Job.AST or Job.SCH or Job.SGE)
+                {
+                    myCloseFirst = false;
+                }
+                else
+                {
+                    myCloseFirst = true;
+                }
+            }
+            else
+            {
+                if(Player.Job is Job.DRK or Job.WAR or Job.GNB or Job.PLD or Job.WHM or Job.AST or Job.SCH or Job.SGE)
+                {
+                    myCloseFirst = true;
+                }
+                else
+                {
+                    myCloseFirst = false;
+                }
+            }
+        }
         List<bool> seq = [SequenceIsClose.SafeSelect(0) == myCloseFirst, SequenceIsClose.SafeSelect(1) != myCloseFirst, SequenceIsClose.SafeSelect(2) == myCloseFirst, SequenceIsClose.SafeSelect(3) != myCloseFirst];
         return seq;
     }
@@ -102,7 +132,7 @@ public unsafe class EX4_Escelons_Fall : SplatoonScript
             var isMyClose = GetMyCloses()[this.NumSwitches];
             var correct = IsSelfClose() == isMyClose;
             Controller.GetElementByName(isMyClose ? $"In" : $"Out")!.Enabled = true;
-            Controller.GetElementByName(isMyClose ? $"In{(correct?"Correct":"Incorrect")}" : $"Out{(correct ? "Correct" : "Incorrect")}")!.Enabled = true;
+            Controller.GetElementByName(isMyClose ? $"In{(correct ? "Correct" : "Incorrect")}" : $"Out{(correct ? "Correct" : "Incorrect")}")!.Enabled = true;
         }
     }
 
@@ -121,11 +151,37 @@ public unsafe class EX4_Escelons_Fall : SplatoonScript
         if(category.EqualsAny(DirectorUpdateCategory.Complete, DirectorUpdateCategory.Recommence, DirectorUpdateCategory.Wipe)) Reset();
     }
 
+    public override void OnStartingCast(uint source, uint castId)
+    {
+        if(castId == this.RoseBloom3rd)
+        {
+            PluginLog.Information($"Next Escelons Need Adjust");
+            AdjustPhase = true;
+        }
+    }
+
+    public override void OnVFXSpawn(uint target, string vfxPath)
+    {
+        if(AdjustPhase && vfxPath.Contains("vfx/lockon/eff/x6fd_shock_lock2v.avfx"))
+        {
+            if(target.TryGetObject(out var obj) && obj is IPlayerCharacter pc && !THShockTargeted)
+            {
+                if(pc.GetJob() is Job.DRK or Job.WAR or Job.GNB or Job.PLD or Job.WHM or Job.AST or Job.SCH or Job.SGE)
+                {
+                    PluginLog.Information($"TH Shock Targeted: {pc.Name.ToString()}");
+                    THShockTargeted = true;
+                }
+            }
+        }
+    }
+
     void Reset()
     {
         this.SequenceIsClose.Clear();
         NumSwitches = 0;
         Controller.GetRegisteredElements().Each(x => x.Value.Enabled = false);
+        AdjustPhase = false;
+        THShockTargeted = false;
     }
 
     Config C => Controller.GetConfig<Config>();

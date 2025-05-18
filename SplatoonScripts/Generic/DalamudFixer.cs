c1ccc80ca441fd83cf1b5707364e00760541920a
Limiana <5073202+Limiana@users.noreply.github.com>
2025-05-18 08:40:22 +0300

    Behold the creation of the Bahamut himself

---
+++ SplatoonScripts/Generic/DalamudFixer.cs
@@ -0,0 +1,23 @@
+﻿using ECommons.Reflection;
+using ImGuiNET;
+using Splatoon.SplatoonScripting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SplatoonScriptsOfficial.Generic;
+public class DalamudFixer : SplatoonScript
+{
+    public override HashSet<uint>? ValidTerritories { get; } = [0];
+
+    public override void OnSettingsDraw()
+    {
+        if(ImGui.Button("Fix DTR bar errors"))
+        {
+            DalamudReflector.GetService("Dalamud.Game.Gui.Dtr.DtrBar").SetFoP("entriesLock", new ReaderWriterLockSlim());
+        }
+    }
+}

a95feb8d105ab0619e3fe9ab5813a45f1d71df27
Limiana <5073202+Limiana@users.noreply.github.com>
2025-09-21 08:58:53 +0300

    Update fof script

--- SplatoonScripts/Duties/Dawntrail/The Futures Rewritten/P1 Fall of Faith EN.cs
+++ SplatoonScripts/Duties/Dawntrail/The Futures Rewritten/P1 Fall of Faith EN.cs
@@ -1,4 +1,5 @@
-﻿using Dalamud.Game.ClientState.Objects.SubKinds;
+﻿using Dalamud.Bindings.ImGui;
+using Dalamud.Game.ClientState.Objects.SubKinds;
 using Dalamud.Game.ClientState.Objects.Types;
 using ECommons;
 using ECommons.Configuration;
@@ -6,24 +7,27 @@ using ECommons.DalamudServices;
 using ECommons.GameHelpers;
 using ECommons.ImGuiMethods;
 using ECommons.Logging;
+using ECommons.MathHelpers;
 using ECommons.Throttlers;
-using Dalamud.Bindings.ImGui;
 using Splatoon.SplatoonScripting;
 using Splatoon.SplatoonScripting.Priority;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using TetherInfo = (uint ObjectID, bool IsFire);
 
 namespace SplatoonScriptsOfficial.Duties.Dawntrail.The_Futures_Rewritten;
 public class P1_Fall_of_Faith_EN : SplatoonScript
 {
     public override HashSet<uint>? ValidTerritories { get; } = [1238];
-    public override Metadata? Metadata => new(2, "NightmareXIV");
+    public override Metadata? Metadata => new(3, "NightmareXIV");
     private Config C => Controller.GetConfig<Config>();
     private List<TetherInfo> Tethers = [];
     private int PlayersRemaining => Svc.Objects.OfType<IPlayerCharacter>().Count(x => x.StatusList.Any(s => s.StatusId == 1051));
     private bool Active = false;
     private bool IsBossCasting => Svc.Objects.OfType<IBattleNpc>().Any(x => x.IsTargetable && x.CastActionId.EqualsAny<uint>(40137, 40140));
+    bool PlayerHadTether = false;
 
     public override void OnSetup()
     {
@@ -37,7 +41,69 @@ public class P1_Fall_of_Faith_EN : SplatoonScript
         {
             Controller.RegisterElementFromCode($"Line{i}", "{\"Name\":\"Line\",\"type\":3,\"refY\":10.0,\"radius\":0.0,\"color\":3372220160,\"fillIntensity\":0.345,\"refActorComparisonType\":2,\"includeRotation\":true,\"FaceMe\":true,\"refActorTetherTimeMin\":0.0,\"refActorTetherTimeMax\":0.0,\"faceplayer\":\"<2>\"}");
         }
+
+        Controller.RegisterElementFromCode("LineSouth", """{"Name":"","type":3,"refY":3.0,"radius":0.0,"color":3371433728,"Filled":false,"fillIntensity":0.345,"thicc":8.0,"refActorObjectID":0,"refActorComparisonType":2}""");
+        Controller.RegisterElementFromCode("LineNorth", """{"Name":"","type":3,"refY":-3.0,"radius":0.0,"color":3371433728,"Filled":false,"fillIntensity":0.345,"thicc":8.0,"refActorObjectID":0,"refActorComparisonType":2}""");
+        Controller.RegisterElementFromCode("LineWest", """{"Name":"","type":3,"refX":-3.0,"radius":0.0,"color":3371433728,"Filled":false,"fillIntensity":0.345,"thicc":8.0,"refActorObjectID":0,"refActorComparisonType":2}""");
+        Controller.RegisterElementFromCode("LineEast", """{"Name":"","type":3,"refX":3.0,"radius":0.0,"color":3371433728,"Filled":false,"fillIntensity":0.345,"thicc":8.0,"refActorObjectID":0,"refActorComparisonType":2}""");
     }
+
+    void DrawGuideOn(IGameObject player, bool isFire)
+    {
+        List<string> lines = [];
+        var direction = GetDirection(player.Position.ToVector2());
+        if(isFire || PlayerHadTether)
+        {
+            var e = Controller.GetElementByName($"Line{direction}");
+            e.Enabled = true;
+            e.refActorObjectID = player.EntityId;
+            e.color = (isFire ? EColor.RedBright : EColor.CyanBright).ToUint();
+        }
+        else
+        {
+            if(direction == Cardinal.North || direction == Cardinal.South)
+            {
+                foreach(var e in ((string[])["LineWest", "LineEast"]).Select(Controller.GetElementByName))
+                {
+                    e.Enabled = true;
+                    e.refActorObjectID = player.EntityId;
+                    e.color = (isFire ? EColor.RedBright : EColor.CyanBright).ToUint();
+                }
+            }
+            else
+            {
+                foreach(var e in ((string[])["LineNorth", "LineSouth"]).Select(Controller.GetElementByName))
+                {
+                    e.Enabled = true;
+                    e.refActorObjectID = player.EntityId;
+                    e.color = (isFire ? EColor.RedBright : EColor.CyanBright).ToUint();
+                }
+            }
+        }
+    }
+
+    enum Cardinal { North, West, South, East }
+
+    private const float CX = 100f;
+    private const float CY = 100f;
+
+    static Cardinal GetDirection(Vector2 point)
+    {
+        float dx = point.X - CX;     
+        float dy = point.Y - CY;   
+
+        float adx = MathF.Abs(dx);
+        float ady = MathF.Abs(dy);
+
+        if(adx > ady)
+            return dx > 0f ? Cardinal.East : Cardinal.West;
+
+        if(ady > adx)
+            return dy > 0f ? Cardinal.South : Cardinal.North;
+
+        return dy > 0f ? Cardinal.South : Cardinal.North;
+    }
+
 
     public override void OnUpdate()
     {
@@ -49,6 +115,10 @@ public class P1_Fall_of_Faith_EN : SplatoonScript
             foreach(var x in Tethers)
             {
                 if(x.ObjectID.GetObject() is IPlayerCharacter pc && !pc.StatusList.Any(s => s.StatusId == 1051)) continue;
+                if(x.ObjectID == Player.Object.EntityId)
+                {
+                    PlayerHadTether = true;
+                }
                 if(cnt < 2)
                 {
                     ActiveTethers.Add(x.ObjectID.GetObject() as IPlayerCharacter);
@@ -60,10 +130,31 @@ public class P1_Fall_of_Faith_EN : SplatoonScript
                     elem.refActorObjectID = x.ObjectID;
                     if(x.ObjectID == Player.Object.EntityId)
                     {
-                        elem.overlayText += " (>>>YOU<<<)";
+                        elem.overlayText += " (>>>Your turn, stay forward<<<)";
                     }
                 }
                 cnt++;
+            }
+            for(var i = 0; i < Tethers.Count; i++)
+            {
+                var x = Tethers[i];
+                int c = 0;
+                if(ActiveTethers.Any(s => s.EntityId == x.ObjectID))
+                {
+                    if(c >= 2) continue;
+                    var isMyTetherExploding = ActiveTethers.Any(s => s?.EntityId == Player.Object.EntityId);
+                    if(!isMyTetherExploding)
+                    {
+                        var counterparty = ActiveTethers.FirstOrDefault(s => s != null && s.EntityId != Player.Object.EntityId && GetDirection(s.Position.ToVector2()) == GetDirection(Player.Position.ToVector2()));
+                        if(counterparty != null)
+                        {
+                            var t = Tethers.FirstOrDefault(s => s.ObjectID == counterparty.EntityId);
+                            PluginLog.Debug($"Drawing on {counterparty}");
+                            DrawGuideOn(counterparty, t.IsFire);
+                        }
+                    }
+                    c++;
+                }
             }
             if(Tethers.TryGetFirst(x => x.ObjectID == Player.Object.EntityId, out var player))
             {
@@ -132,6 +223,7 @@ public class P1_Fall_of_Faith_EN : SplatoonScript
     public override void OnReset()
     {
         Active = false;
+        PlayerHadTether = false;
         Tethers.Clear();
     }
 
@@ -144,6 +236,7 @@ public class P1_Fall_of_Faith_EN : SplatoonScript
             ImGui.Checkbox("Active", ref Active);
             ImGuiEx.Text($"Tethers: \n{Tethers.Select(x => $"{x.ObjectID.GetObject()} - fire: {x.IsFire}").Print("\n")}");
             ImGuiEx.Text($"Players: {PlayersRemaining}");
+            ImGuiEx.Text(Svc.Objects.OfType<IPlayerCharacter>().Select(x => $"{x.GetNameWithWorld()} - {GetDirection(x.Position.ToVector2())}").Print("\n"));
         }
     }
 

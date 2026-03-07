25b4ed6ab8ec6c4ec90759fc140b4296b1c5fc8f
Limiana <5073202+Limiana@users.noreply.github.com>
2026-03-07 10:00:29 +0300

    Update scripts

---
+++ SplatoonScripts/Duties/Dawntrail/Another Merchants Take B1 Sunken Treasure.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using ECommons.Configuration;
+using ECommons.DalamudServices;
+using ECommons.GameFunctions;
+using ECommons.Hooks.ActionEffectTypes;
+using ECommons.ImGuiMethods;
+using ECommons.Logging;
+using ECommons.Schedulers;
+using FFXIVClientStructs.FFXIV.Client.Game.Group;
+using Splatoon;
+using Splatoon.Memory;
+using Splatoon.SplatoonScripting;
+using Splatoon.SplatoonScripting.Priority;
+using static Splatoon.Splatoon;
+using static Dalamud.Bindings.ImGui.ImGui;
+
+namespace SplatoonScriptsOfficial.Duties.Dawntrail;
+
+public class Another_Merchants_Take_B1_Sunken_Treasure : SplatoonScript
+{
+    public override HashSet<uint> ValidTerritories => [1317,];
+    public override Metadata? Metadata => new(1, "redmoon");
+
+    private class ObjectData
+    {
+        public uint EntityId = 0;
+        public uint BaseId = 0;
+        public Vector3 Position = Vector3.Zero;
+        public bool IsBroke = false;
+    }
+
+    private readonly List<ObjectData> _objectDataList = [];
+
+    public override void OnSetup()
+    {
+        for (var i = 0; i < 5; i++) Controller.RegisterElement($"aoe{i}", new Element(0));
+    }
+
+    public override void OnActionEffectEvent(ActionEffectSet set)
+    {
+        var castId = set.Action?.RowId ?? 0;
+        if (castId == 45851) { }
+    }
+
+    public override void OnObjectCreation(IntPtr newObjectPtr)
+    {
+        Controller.Schedule(() =>
+        {
+            var obj = Svc.Objects.FirstOrDefault(x => x.Address == newObjectPtr);
+            if (obj == null || (obj.BaseId != 2015004 && obj.BaseId != 2015005)) return;
+            _objectDataList.Add(new ObjectData
+            {
+                EntityId = obj.EntityId,
+                BaseId = obj.BaseId,
+                Position = obj.Position,
+                IsBroke = false,
+            });
+        }, 0);
+    }
+
+    public override void OnObjectEffect(uint target, ushort data1, ushort data2)
+    {
+        if (_objectDataList.Count == 0) return;
+        if (data1 == 16 && data2 == 32)
+        {
+            var obj = _objectDataList.FirstOrDefault(x => x.EntityId == target);
+            if (obj == null) return;
+            obj.IsBroke = true;
+        }
+        else if (data1 == 4 && data2 == 8)
+        {
+            var index = _objectDataList.FindIndex(x => x.EntityId == target);
+            if (index == -1) return;
+            _objectDataList.RemoveAt(index);
+        }
+    }
+
+    public override void OnUpdate()
+    {
+        Controller.Hide();
+        if (_objectDataList.Count == 0) return;
+
+        // IsBroke == false のインスタンスはsortで一番後にする
+        if (_objectDataList.Count(x => x.IsBroke) == 5) _objectDataList.Sort((x, y) => !x.IsBroke ? 1 : -1);
+
+        var index = 0;
+        foreach (var obj in _objectDataList)
+        {
+            if (!obj.IsBroke) continue;
+            if (Controller.TryGetElementByName($"aoe{index}", out var e))
+            {
+                e.radius = obj.BaseId == 2015004 ? 18f : 4f;
+                e.Donut = obj.BaseId == 2015004 ? 0f : 16f;
+                e.Enabled = true;
+                e.SetRefPosition(obj.Position);
+            }
+
+            index++;
+            if (index == 5) break;
+        }
+    }
+
+    public override void OnReset()
+    {
+        _objectDataList.Clear();
+        Controller.Hide();
+    }
+
+    public override void OnSettingsDraw()
+    {
+        // Debug
+        if (!ImGuiEx.CollapsingHeader("Debug")) return;
+        foreach (var obj in _objectDataList)
+            ImGuiEx.Text($"Entity: {obj.EntityId} Base: {obj.BaseId} Pos: {obj.Position} IsBroke: {obj.IsBroke}");
+        Separator();
+        Text("Elements:");
+        foreach (var e in Controller.GetRegisteredElements())
+            ImGuiEx.Text(
+                $"{e.Key}: Enabled={e.Value.Enabled} Pos=({e.Value.refX}, {e.Value.refZ}, {e.Value.refY})");
+    }
+}
--- SplatoonScripts/Duties/Dawntrail/Another Merchants Tale B1 Echoed Serenade.cs
+++ SplatoonScripts/Duties/Dawntrail/Another Merchants Tale B1 Echoed Serenade.cs
@@ -1,14 +1,9 @@
-﻿using System.Collections.Generic;
-using System.Numerics;
-using ECommons;
-using ECommons.Configuration;
+﻿using System;
+using System.Collections.Generic;
 using ECommons.Hooks.ActionEffectTypes;
 using ECommons.ImGuiMethods;
-using Splatoon;
 using Splatoon.Memory;
 using Splatoon.SplatoonScripting;
-using Splatoon.SplatoonScripting.Priority;
-using static Splatoon.Splatoon;
 using static Dalamud.Bindings.ImGui.ImGui;
 
 namespace SplatoonScriptsOfficial.Duties.Dawntrail;
@@ -16,217 +11,183 @@ namespace SplatoonScriptsOfficial.Duties.Dawntrail;
 public class Another_Merchants_Tale_B1_Echoed_Serenade : SplatoonScript
 {
     public override HashSet<uint> ValidTerritories => [1317,];
-    public override Metadata Metadata => new(1, "redmoon");
+    public override Metadata? Metadata => new(2, "redmoon");
 
     private enum MonsterRotation
     {
-        seahorse,
-        chocobo,
-        crab,
-        puffer,
+        Seahorse,
+        Chocobo,
+        Crab,
+        Puffer,
+        Turtle,
     }
 
-    private List<MonsterRotation> _rotations = [];
+    private readonly List<MonsterRotation> _rotations = [];
     private bool _isActivated = false;
-    private int _castCount = 0;
-    private Config C => Controller.GetConfig<Config>();
-
-    private readonly Vector3 center = new(375f, -29.5f, 530f);
+    private int _rotationIndex = 0;
+    private int _gimmickCount = 0;
+    private long _lockNextTime = 0L;
 
     public override void OnSetup()
     {
-        if(C.PlayerData.PriorityLists.Count == 0)
-        {
-            C.PlayerData.PriorityLists.Add(new PriorityList
-            {
-                IsRole = true,
-                List =
-                {
-                    new JobbedPlayer { Role = RolePosition.T1, },
-                    new JobbedPlayer { Role = RolePosition.H1, },
-                    new JobbedPlayer { Role = RolePosition.M1, },
-                    new JobbedPlayer { Role = RolePosition.R1, },
-                },
-            });
-        }
-
         Controller.RegisterElementFromCode("seahorse",
             """{"Name":"","type":3,"refY":40.0,"radius":4.0,"refActorDataID":19098,"refActorComparisonType":3,"includeRotation":true}""");
-
         Controller.RegisterElementFromCode("chocobo",
             """{"Name":"","type":4,"radius":45.0,"coneAngleMin":-30,"coneAngleMax":30,"refActorDataID":19102,"refActorComparisonType":3,"includeRotation":true}""");
         Controller.RegisterElementFromCode("crab",
             """{"Name":"","type":3,"refY":40.0,"radius":4.0,"refActorDataID":19100,"refActorComparisonType":3,"includeRotation":true}""");
         Controller.RegisterElementFromCode("puffer",
             """{"Name":"","type":4,"refY":40.0,"radius":20.0,"coneAngleMin":-90,"coneAngleMax":90,"refActorDataID":19101,"refActorComparisonType":3,"includeRotation":true}""");
+        Controller.RegisterElementFromCode("turtle",
+            """{"Name":"","type":3,"refY":40.0,"radius":4.0,"refActorDataID":19099,"refActorComparisonType":3,"includeRotation":true}""");
 
-        Controller.RegisterElement("guide", new Element(0)
-        {
-            radius = 0.35f,
-            Donut = 0.2f,
-            fillIntensity = 1f,
-        });
+        Controller.RegisterElementFromCode("seahorsePre",
+            """{"Name":"","type":3,"refY":40.0,"radius":3.8,"color":3355508719,"Filled":false,"fillIntensity":0.345,"thicc":20.0,"refActorDataID":19098,"refActorComparisonType":3,"includeRotation":true}""");
+        Controller.RegisterElementFromCode("chocoboPre",
+            """{"Name":"","type":4,"refY":40.0,"radius":19.0,"coneAngleMin":-30,"coneAngleMax":30,"color":3355508719,"Filled":false,"fillIntensity":0.345,"thicc":20.0,"refActorDataID":19102,"refActorComparisonType":3,"includeRotation":true}""");
+        Controller.RegisterElementFromCode("crabPre",
+            """{"Name":"","type":3,"refY":40.0,"radius":3.8,"color":3355508719,"Filled":false,"fillIntensity":0.345,"thicc":20.0,"refActorDataID":19100,"refActorComparisonType":3,"includeRotation":true}""");
+        Controller.RegisterElementFromCode("pufferPre",
+            """{"Name":"","type":4,"refY":40.0,"radius":19.0,"coneAngleMin":-90,"coneAngleMax":90,"color":3355508719,"Filled":false,"fillIntensity":0.345,"thicc":20.0,"refActorDataID":19101,"refActorComparisonType":3,"includeRotation":true}""");
+        Controller.RegisterElementFromCode("turtlePre",
+            """{"Name":"","type":3,"refY":40.0,"radius":3.8,"color":3355508719,"Filled":false,"fillIntensity":0.345,"thicc":20.0,"refActorDataID":19099,"refActorComparisonType":3,"includeRotation":true}""");
     }
 
     public override unsafe void OnStartingCast(uint sourceId, PacketActorCast* packet)
     {
-        if(packet->ActionID == 45771)
+        if (packet->ActionID == 45771)
         {
-            _rotations.Clear();
+            if (_gimmickCount != 2) _rotations.Clear();
             _isActivated = true;
         }
     }
 
     public override void OnActionEffectEvent(ActionEffectSet set)
     {
-        if(!_isActivated) return;
+        if (!_isActivated) return;
         var castId = set.Action?.RowId ?? 0;
 
-        if(castId == 45839) // seahorse
+        if (castId is 45839 or 45840 or 45841 or 45842 or 45843 && _lockNextTime == 0)
         {
-            if(_castCount == 0) _rotations.RemoveAt(0);
-            _castCount++;
-            if(_castCount == 4) _castCount = 0;
-            if(_rotations.Count == 0)
-                OnReset();
+            _rotationIndex++;
+            _lockNextTime = Environment.TickCount64 + 300;
         }
-        else if(castId == 45843) // chocobo
+
+        if (_rotationIndex == 4)
         {
-            if(_castCount == 0) _rotations.RemoveAt(0);
-            _castCount++;
-            if(_castCount == 2) _castCount = 0;
-            if(_rotations.Count == 0)
-                OnReset();
-        }
-        else if(castId == 45841) // crab
-        {
-            if(_castCount == 0) _rotations.RemoveAt(0);
-            _castCount++;
-            if(_castCount == 4) _castCount = 0;
-            if(_rotations.Count == 0)
-                OnReset();
-        }
-        else if(castId == 45842) // puffer
-        {
-            if(_castCount == 0) _rotations.RemoveAt(0);
-            _castCount++;
-            if(_castCount == 2) _castCount = 0;
-            if(_rotations.Count == 0)
-                OnReset();
+            _isActivated = false;
+            _rotationIndex = 0;
+            if (_gimmickCount != 1) _rotations.Clear();
+            _gimmickCount++;
+            Controller.Hide();
         }
     }
 
     public override void OnVFXSpawn(uint target, string vfxPath)
     {
-        if(!_isActivated) return;
-        if(vfxPath.Contains("vfx/common/eff/m0941_seahorse_c0h.avfx"))
-            _rotations.Add(MonsterRotation.seahorse);
-        else if(vfxPath.Contains("vfx/common/eff/m0941_chocobo_c0h.avfx"))
-            _rotations.Add(MonsterRotation.chocobo);
-        else if(vfxPath.Contains("vfx/common/eff/m0941_crab_c0h.avfx"))
-            _rotations.Add(MonsterRotation.crab);
-        else if(vfxPath.Contains("vfx/common/eff/m0941_puffer_c0h.avfx"))
-            _rotations.Add(MonsterRotation.puffer);
+        if (!_isActivated) return;
+        if (vfxPath.Contains("vfx/common/eff/m0941_seahorse_c0h.avfx"))
+            _rotations.Add(MonsterRotation.Seahorse);
+        else if (vfxPath.Contains("vfx/common/eff/m0941_chocobo_c0h.avfx"))
+            _rotations.Add(MonsterRotation.Chocobo);
+        else if (vfxPath.Contains("vfx/common/eff/m0941_crab_c0h.avfx"))
+            _rotations.Add(MonsterRotation.Crab);
+        else if (vfxPath.Contains("vfx/common/eff/m0941_puffer_c0h.avfx"))
+            _rotations.Add(MonsterRotation.Puffer);
+        else if (vfxPath.Contains("vfx/common/eff/m0941_turtle_c0h.avfx"))
+            _rotations.Add(MonsterRotation.Turtle);
     }
 
     public override void OnUpdate()
     {
-        if(!_isActivated) return;
+        if (!_isActivated) return;
         Controller.Hide();
-        switch(_rotations[0])
+        if (_lockNextTime != 0 && Environment.TickCount64 >= _lockNextTime) _lockNextTime = 0;
+        if (_rotations.Count == 0) return;
+        switch (_rotations[_rotationIndex])
         {
-            case MonsterRotation.seahorse:
-                {
-                    if(Controller.TryGetElementByName("seahorse", out var seahorse))
-                        seahorse.Enabled = true;
-                    // var guidePos = MirroringByIndex(new Vector3(387.644f, -29.5f, 510.6f),
-                    //     C.PlayerData.GetOwnIndex(_ => true));
-
-                    // if (Controller.TryGetElementByName("guide", out var guide))
-                    // {
-                    //     guide.SetRefPosition(guidePos);
-                    //     guide.Enabled = true;
-                    // }
-
-                    break;
-                }
-            case MonsterRotation.chocobo:
-                {
-                    if(Controller.TryGetElementByName("chocobo", out var chocobo))
-                        chocobo.Enabled = true;
-                    break;
-                }
-            case MonsterRotation.crab:
-                {
-                    if(Controller.TryGetElementByName("crab", out var crab))
-                        crab.Enabled = true;
-                    break;
-                }
-            case MonsterRotation.puffer:
-                {
-                    if(Controller.TryGetElementByName("puffer", out var puffer))
-                        puffer.Enabled = true;
-                    break;
-                }
-            default:
+            case MonsterRotation.Seahorse:
+            {
+                if (Controller.TryGetElementByName("seahorse", out var seahorse)) seahorse.Enabled = true;
                 break;
+            }
+            case MonsterRotation.Chocobo:
+            {
+                if (Controller.TryGetElementByName("chocobo", out var chocobo)) chocobo.Enabled = true;
+                break;
+            }
+            case MonsterRotation.Crab:
+            {
+                if (Controller.TryGetElementByName("crab", out var crab)) crab.Enabled = true;
+                break;
+            }
+            case MonsterRotation.Puffer:
+            {
+                if (Controller.TryGetElementByName("puffer", out var puffer)) puffer.Enabled = true;
+                break;
+            }
+            case MonsterRotation.Turtle:
+            {
+                if (Controller.TryGetElementByName("turtle", out var turtle)) turtle.Enabled = true;
+                break;
+            }
         }
 
-        // {
-        //     if (Controller.TryGetElementByName("guide", out var guide) && guide.Enabled) // Rainbow
-        //         guide.color = GradientColor.Get(0xFF00FF00.ToVector4(), 0xFF0000FF.ToVector4()).ToUint();
-        // }
+        if (_rotations.Count <= 1 || _rotationIndex == 3) return;
+        switch (_rotations[_rotationIndex + 1]) // Pre
+        {
+            case MonsterRotation.Seahorse:
+            {
+                if (Controller.TryGetElementByName("seahorsePre", out var seahorsePre)) seahorsePre.Enabled = true;
+                break;
+            }
+            case MonsterRotation.Chocobo:
+            {
+                if (Controller.TryGetElementByName("chocoboPre", out var chocoboPre)) chocoboPre.Enabled = true;
+                break;
+            }
+            case MonsterRotation.Crab:
+            {
+                if (Controller.TryGetElementByName("crabPre", out var crabPre)) crabPre.Enabled = true;
+                break;
+            }
+            case MonsterRotation.Puffer:
+            {
+                if (Controller.TryGetElementByName("pufferPre", out var pufferPre)) pufferPre.Enabled = true;
+                break;
+            }
+            case MonsterRotation.Turtle:
+            {
+                if (Controller.TryGetElementByName("turtlePre", out var turtlePre)) turtlePre.Enabled = true;
+                break;
+            }
+        }
     }
 
     public override void OnReset()
     {
         _rotations.Clear();
         _isActivated = false;
-        _castCount = 0;
         Controller.Hide();
-    }
-
-    public class PriorityData4 : PriorityData
-    {
-        public override int GetNumPlayers() => 4;
-    }
-
-    public class Config : IEzConfig
-    {
-        public PriorityData4 PlayerData = new();
+        _gimmickCount = 0;
+        _rotationIndex = 0;
     }
 
     public override void OnSettingsDraw()
     {
-        C.PlayerData.Draw();
-
-        if(ImGuiEx.CollapsingHeader("Debug"))
-        {
-            Text($"_isActivated: {_isActivated}");
-            Text($"_castCount: {_castCount}");
-            Separator();
-            ImGuiEx.Text("_rotations:");
-            foreach(var r in _rotations) ImGuiEx.Text(r.ToString());
-        }
-    }
-
-    private Vector3 MirroringByIndex(Vector3 original, int index)
-    {
-        return index switch
-        {
-            // 0 Mirror X
-            0 => original with { X = center.X - (original.X - center.X), },
-            // 1 Mirror Z
-            1 => original with { Z = center.Z - (original.Z - center.Z), },
-            // 2 No Mirror
-            2 => original,
-            // 3 Mirror XZ
-            3 => original with
-            {
-                X = center.X - (original.X - center.X),
-                Z = center.Z - (original.Z - center.Z),
-            },
-            _ => original,
-        };
+        // Debug
+        if (!ImGuiEx.CollapsingHeader("Debug")) return;
+        Text($"_isActivated: {_isActivated}");
+        Text($"_rotations.Count: {_rotations.Count}");
+        InputInt("_rotationIndex", ref _rotationIndex);
+        InputInt("_gimmickCount", ref _gimmickCount);
+        Separator();
+        ImGuiEx.Text("_rotations:");
+        foreach (var r in _rotations) ImGuiEx.Text(r.ToString());
+        Separator();
+        Text("Elements:");
+        foreach (var e in Controller.GetRegisteredElements())
+            ImGuiEx.Text(
+                $"{e.Key}: Enabled={e.Value.Enabled} Pos=({e.Value.refX}, {e.Value.refZ}, {e.Value.refY})");
     }
 }

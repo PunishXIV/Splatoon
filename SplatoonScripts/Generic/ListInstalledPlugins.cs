a1bcbaa8e9736bca266073bbba7bf9e7f483004b
nightmarexivplugins@gmail.com <5073202+Limiana@users.noreply.github.com>
2026-05-14 19:40:34 +0300

    Update

--- SplatoonScripts/Generic/ListInstalledPlugins.cs
+++ SplatoonScripts/Generic/ListInstalledPlugins.cs
@@ -11,11 +11,11 @@ namespace SplatoonScriptsOfficial.Generic;
 
 public class ListInstalledPlugins : SplatoonScript
 {
-    public override Metadata Metadata { get; } = new(1, "NightmareXIV");
+    public override Metadata Metadata { get; } = new(2, "NightmareXIV");
     public override HashSet<uint>? ValidTerritories { get; } = [9999];
 
     public override void OnSettingsDraw()
     {
-        ImGuiEx.TextCopy(Svc.PluginInterface.InstalledPlugins.Where(x => x.IsLoaded).Select(x => $"{x.Name} v{x.Version} [{x.InternalName}] from {x.Manifest.RepoUrl?.NullWhenEmpty() ?? "Main repo"}").Print("\n"));
+        ImGuiEx.TextCopy(Svc.PluginInterface.InstalledPlugins.Where(x => x.IsLoaded).OrderBy(x => x.Name).Select(x => $"{x.Name} v{x.Version} [{x.InternalName}] from {x.Manifest.InstalledFromUrl?.NullWhenEmpty() ?? "DEV"}").Print("\n"));
     }
 }

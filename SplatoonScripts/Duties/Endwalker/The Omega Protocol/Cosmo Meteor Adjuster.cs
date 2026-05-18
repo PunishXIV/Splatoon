b8867f6c972dbe8e2a51680f310f42f3bba6c8c8
exatrines <exatrines@gmail.com>
2026-05-18 20:05:14 +0900

    fix top script use priority-data

--- SplatoonScripts/Duties/Endwalker/The Omega Protocol/Cosmo Meteor Adjuster.cs
+++ SplatoonScripts/Duties/Endwalker/The Omega Protocol/Cosmo Meteor Adjuster.cs
@@ -1,34 +1,28 @@
-﻿using Dalamud.Game.ClientState.Objects.SubKinds;
+﻿using Dalamud.Bindings.ImGui;
+using Dalamud.Game.ClientState.Objects.SubKinds;
 using ECommons;
 using ECommons.Configuration;
 using ECommons.DalamudServices;
-using ECommons.ExcelServices;
 using ECommons.GameFunctions;
 using ECommons.GameHelpers;
-using Player = ECommons.GameHelpers.LegacyPlayer.Player;
-using ECommons.GameHelpers.LegacyPlayer;
 using ECommons.Hooks.ActionEffectTypes;
 using ECommons.ImGuiMethods;
-using ECommons.Logging;
-using ECommons.PartyFunctions;
 using FFXIVClientStructs.FFXIV.Client.Game.Object;
-using FFXIVClientStructs.FFXIV.Client.UI.Info;
-using Dalamud.Bindings.ImGui;
 using Splatoon.SplatoonScripting;
+using Splatoon.SplatoonScripting.Priority;
+using static Splatoon.Splatoon;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Numerics;
 
-using ECommons.DalamudServices.Legacy;
-
 namespace SplatoonScriptsOfficial.Duties.Endwalker.The_Omega_Protocol;
 internal unsafe class Cosmo_Meteor_Adjuster : SplatoonScript
 {
     #region PublicDef
     public override HashSet<uint> ValidTerritories => [1122];
-    public override Metadata? Metadata => new(2, "Redmoon");
+    public override Metadata? Metadata => new(3, "Redmoon, mirage");
     #endregion
 
     #region PrivateDef
@@ -36,10 +30,6 @@ internal unsafe class Cosmo_Meteor_Adjuster : SplatoonScript
     {
         public IPlayerCharacter character;
         public bool mine = false;
-        public float NorthDistance = 0;
-        public float EastDistance = 0;
-        public float WestDistance = 0;
-        public float SouthDistance = 0;
 
         public FlareContainer(IPlayerCharacter character, bool mine)
         {
@@ -65,16 +55,16 @@ internal unsafe class Cosmo_Meteor_Adjuster : SplatoonScript
         North = 1,
         South = 2
     }
-    private readonly Job[] RangedDps = { Job.BRD, Job.MCH, Job.DNC };
+
+    private static readonly string[] NorthFlareElements = ["FlareEast", "FlareSouth", "FlareWest"];
+
     private StackPos _stackPos = StackPos.Undefined;
     private List<FlareContainer> _flarePos = [];
     private bool _gimmickActive = false;
     private bool _isFlareMine = false;
-    private bool _isFindRange = false;
     private GameObjectManager* _gom = GameObjectManager.Instance();
     private (string, string)[] _flareData = new (string, string)[3];
     private Config C => Controller.GetConfig<Config>();
-    private readonly ImGuiEx.RealtimeDragDrop<Job> DragDrop = new("DragDropJob", x => x.ToString());
     #endregion
 
     #region public
@@ -96,24 +86,15 @@ internal unsafe class Cosmo_Meteor_Adjuster : SplatoonScript
             if(vfxPath == VfxPath.Flare)
             {
                 _flarePos.Add(new FlareContainer(character, false));
-                if(character.Address == Svc.ClientState.LocalPlayer.Address)
+                if(BasePlayer != null && character.GameObjectId == BasePlayer.GameObjectId)
                 {
                     _flarePos.Last().mine = true;
                     _isFlareMine = true;
                 }
 
-                if(RangedDps.Contains(character.GetJob()))
-                {
-                    _isFindRange = true;
-                    _stackPos = StackPos.South;
-                }
-                else if(_flarePos.Count >= 3 && _isFindRange == false)
-                {
-                    _stackPos = StackPos.North;
-                }
-
                 if(_flarePos.Count >= 3)
                 {
+                    ResolveStackPos();
                     Controller.GetRegisteredElements().Each(x => x.Value.Enabled = false);
                     ArbitPosition();
 
@@ -177,94 +158,24 @@ internal unsafe class Cosmo_Meteor_Adjuster : SplatoonScript
         _isFlareMine = false;
         _gimmickActive = false;
         _flareData = new (string, string)[3];
-        _isFindRange = false;
         Controller.GetRegisteredElements().Each(x => x.Value.Enabled = false);
     }
 
     public class Config : IEzConfig
     {
         public bool Debug = false;
-        public List<string[]> clockWisePriorities = [];
-
-        public List<Job> Jobs =
-        [
-            Job.PLD,
-            Job.WAR,
-            Job.DRK,
-            Job.GNB,
-            Job.WHM,
-            Job.SCH,
-            Job.AST,
-            Job.SGE,
-            Job.VPR,
-            Job.DRG,
-            Job.MNK,
-            Job.SAM,
-            Job.RPR,
-            Job.NIN,
-            Job.BRD,
-            Job.MCH,
-            Job.DNC,
-            Job.BLM,
-            Job.SMN,
-            Job.RDM,
-            Job.PCT
-        ];
+        public PriorityData PriorityData = new()
+        {
+            Name = "Flare Priority",
+            Description = "Clockwise from North (Range LB Player). Position 1 is Range LB; if they have a flare, party stacks South.",
+        };
     }
 
     public override void OnSettingsDraw()
     {
-        ImGui.Text("# How to determine left/right priority : ");
-        ImGui.SameLine();
-        if(ImGui.SmallButton("Test"))
-        {
-            if(TryGetPriorityList(out var list))
-            {
-                DuoLog.Information($"Success: priority list {list.Print()}");
-            }
-            else
-            {
-                DuoLog.Warning($"Could not get priority list");
-            }
-        }
-        var toRem = -1;
-        for(var i = 0; i < C.clockWisePriorities.Count; i++)
-        {
-            if(DrawPrioList(i))
-            {
-                toRem = i;
-            }
-        }
-        if(toRem != -1)
-        {
-            C.clockWisePriorities.RemoveAt(toRem);
-        }
-        if(ImGui.Button("Create new priority list"))
-        {
-            C.clockWisePriorities.Add(new string[] { "", "", "", "", "", "", "", "" });
-        }
-
-        if(ImGuiEx.CollapsingHeader("Option"))
-        {
-            DragDrop.Begin();
-            foreach(var job in C.Jobs)
-            {
-                DragDrop.NextRow();
-                ImGui.Text(job.ToString());
-                ImGui.SameLine();
-
-                if(ThreadLoadImageHandler.TryGetIconTextureWrap((uint)job.GetIcon(), false, out var texture))
-                {
-                    ImGui.Image(texture.Handle, new Vector2(24f));
-                    ImGui.SameLine();
-                }
-
-                ImGui.SameLine();
-                DragDrop.DrawButtonDummy(job, C.Jobs, C.Jobs.IndexOf(job));
-            }
-
-            DragDrop.End();
-        }
+        ImGui.Text("Flare Priority");
+        ImGui.Text("Clockwise from North (Range LB Player)");
+        C.PriorityData.Draw();
 
         if(ImGui.CollapsingHeader("Debug"))
         {
@@ -283,10 +194,6 @@ internal unsafe class Cosmo_Meteor_Adjuster : SplatoonScript
             {
                 entries.Add(new ImGuiEx.EzTableEntry("Name", () => ImGui.Text(character.character.Name.ToString())));
                 entries.Add(new ImGuiEx.EzTableEntry("Mine", () => ImGui.Text(character.mine.ToString())));
-                entries.Add(new ImGuiEx.EzTableEntry("East", () => ImGui.Text(character.EastDistance.ToString())));
-                entries.Add(new ImGuiEx.EzTableEntry("West", () => ImGui.Text(character.WestDistance.ToString())));
-                entries.Add(new ImGuiEx.EzTableEntry("North", () => ImGui.Text(character.NorthDistance.ToString())));
-                entries.Add(new ImGuiEx.EzTableEntry("South", () => ImGui.Text(character.SouthDistance.ToString())));
             }
             ImGuiEx.EzTable(entries);
         }
@@ -296,134 +203,87 @@ internal unsafe class Cosmo_Meteor_Adjuster : SplatoonScript
     #region private
     private bool TryGetPriorityList([NotNullWhen(true)] out List<IPlayerCharacter> values)
     {
-        foreach(var p in C.clockWisePriorities)
+        var players = C.PriorityData.GetPlayers(_ => true);
+        if(players == null || players.Count == 0)
         {
-            var valid = true;
-            var l = FakeParty.Get().Select(x => x.Name.ToString()).ToHashSet();
-            foreach(var x in p)
-            {
-                if(!l.Remove(x))
-                {
-                    valid = false;
-                    break;
-                }
-            }
-            if(valid)
-            {
-                values = FakeParty.Get().ToList().OrderBy(x => Array.IndexOf(p, x.Name.ToString())).ToList();
-                return true;
-            }
+            values = [];
+            return false;
         }
-        values = [];
-        return false;
+
+        values = players
+            .Select(x => x.IGameObject as IPlayerCharacter)
+            .Where(x => x != null)
+            .Cast<IPlayerCharacter>()
+            .ToList();
+
+        return values.Count > 0;
     }
 
-    private bool DrawPrioList(int num)
+    private IPlayerCharacter? GetRangeLbPlayer()
     {
-        var prio = C.clockWisePriorities[num];
-        ImGuiEx.Text($"# Priority list {num + 1}");
-        ImGui.PushID($"prio{num}");
-        ImGuiEx.Text($"    North (Ranged DPS) ClockWise");
-        for(var i = 0; i < prio.Length; i++)
-        {
-            ImGui.PushID($"prio{num}element{i}");
-            ImGui.SetNextItemWidth(200);
-            ImGui.InputText($"Player {i + 1}", ref prio[i], 50);
-            ImGui.SameLine();
-            ImGui.SetNextItemWidth(150);
-            if(ImGui.BeginCombo("##partysel", "Select from party"))
-            {
-                foreach(var x in FakeParty.Get().Select(x => x.Name.ToString())
-                             .Union(UniversalParty.Members.Select(x => x.Name)).ToHashSet())
-                    if(ImGui.Selectable(x))
-                        prio[i] = x;
-                ImGui.EndCombo();
-            }
-            ImGui.PopID();
-        }
-        ImGuiEx.Text($"    NorthWest");
-        if(ImGui.Button("Delete this list (ctrl+click)") && ImGui.GetIO().KeyCtrl)
-        {
-            return true;
-        }
+        if(!TryGetPriorityList(out var priorityList) || priorityList.Count == 0)
+            return null;
 
-        ImGui.SameLine();
-        if(ImGui.Button("Fill by job"))
-        {
-            HashSet<(string, Job)> party = [];
-            foreach(var x in FakeParty.Get())
-                party.Add((x.Name.ToString(), x.GetJob()));
+        return priorityList[0];
+    }
 
-            var proxy = InfoProxyCrossRealm.Instance();
-            for(var i = 0; i < proxy->GroupCount; i++)
-            {
-                var group = proxy->CrossRealmGroups[i];
-                for(var c = 0; c < proxy->CrossRealmGroups[i].GroupMemberCount; c++)
-                {
-                    var x = group.GroupMembers[c];
-                    party.Add((x.Name.Read(), (Job)x.ClassJobId));
-                }
-            }
-
-            var index = 0;
-            foreach(var job in C.Jobs.Where(job => party.Any(x => x.Item2 == job)))
-            {
-                prio[index] = party.First(x => x.Item2 == job).Item1;
-                index++;
-            }
-
-            for(var i = index; i < prio.Length; i++)
-                prio[i] = "";
-        }
-        ImGuiEx.Tooltip("The list is populated based on the job.\nYou can adjust the priority from the option header.");
-
-        ImGui.PopID();
-        return false;
+    private void ResolveStackPos()
+    {
+        var rangePlayer = GetRangeLbPlayer();
+        if(rangePlayer != null && _flarePos.Any(x => x.character.Address == rangePlayer.Address))
+            _stackPos = StackPos.South;
+        else
+            _stackPos = StackPos.North;
     }
 
     private void ArbitPosition()
     {
-        string[] northElementsArray = { "FlareEast", "FlareSouth", "FlareWest" };
-        string[] southElementsArray = { "FlareNorth", "FlareEast", "FlareWest" };
+        if(!TryGetPriorityList(out var priorityList))
+            return;
 
-        List<IPlayerCharacter> priorityList = [];
+        var flarePlayers = priorityList
+            .Where(p => _flarePos.Any(f => f.character.Address == p.Address))
+            .ToList();
 
-        if(!TryGetPriorityList(out priorityList)) return;
+        if(flarePlayers.Count < 3)
+            return;
+
+        _flareData = new (string, string)[3];
+        var dataIndex = 0;
 
         if(_stackPos == StackPos.North)
         {
-            var i = 0;
-            foreach(var priorityMember in priorityList)
-            {
-                if(_flarePos.Any(x => x.character.Address == priorityMember.Address))
-                {
-                    _flareData[i] = (northElementsArray[i], priorityMember.Name.ToString());
-                    if(Svc.ClientState.LocalPlayer.Address == priorityMember.Address)
-                    {
-                        Controller.GetElementByName(northElementsArray[i]).Enabled = true;
-                        Controller.GetElementByName(northElementsArray[i]).tether = true;
-                    }
-                    i++;
-                }
-            }
+            for(var i = 0; i < 3; i++)
+                AssignFlareElement(NorthFlareElements[i], flarePlayers[i], ref dataIndex);
         }
         else if(_stackPos == StackPos.South)
         {
-            var i = 0;
-            foreach(var priorityMember in priorityList)
-            {
-                if(_flarePos.Any(x => x.character.Address == priorityMember.Address))
-                {
-                    _flareData[i] = (southElementsArray[i], priorityMember.Name.ToString());
-                    if(Svc.ClientState.LocalPlayer.Address == priorityMember.Address)
-                    {
-                        Controller.GetElementByName(southElementsArray[i]).Enabled = true;
-                        Controller.GetElementByName(southElementsArray[i]).tether = true;
-                    }
-                    i++;
-                }
-            }
+            var rangePlayer = GetRangeLbPlayer();
+            var nonRange = flarePlayers
+                .Where(p => rangePlayer == null || p.Address != rangePlayer.Address)
+                .ToList();
+
+            if(nonRange.Count >= 1)
+                AssignFlareElement("FlareWest", nonRange[0], ref dataIndex);
+            if(rangePlayer != null && flarePlayers.Any(p => p.Address == rangePlayer.Address))
+                AssignFlareElement("FlareNorth", rangePlayer, ref dataIndex);
+            if(nonRange.Count >= 2)
+                AssignFlareElement("FlareEast", nonRange[1], ref dataIndex);
         }
+    }
+
+    private void AssignFlareElement(string elementName, IPlayerCharacter player, ref int dataIndex)
+    {
+        if(dataIndex < _flareData.Length)
+            _flareData[dataIndex] = (elementName, player.Name.ToString());
+
+        if(BasePlayer != null && player.GameObjectId == BasePlayer.GameObjectId)
+        {
+            Controller.GetElementByName(elementName).Enabled = true;
+            Controller.GetElementByName(elementName).tether = true;
+        }
+
+        dataIndex++;
     }
     #endregion
 }

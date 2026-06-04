0aa55421f33f255d83dfee055f8691415a41dc5e
Limiana <5073202+Limiana@users.noreply.github.com>
2026-06-05 02:39:42 +0300

    Update P2_Forsaken_beta.cs

--- SplatoonScripts/Duties/Dawntrail/Dancing Mad/P2_Forsaken_beta.cs
+++ SplatoonScripts/Duties/Dawntrail/Dancing Mad/P2_Forsaken_beta.cs
@@ -195,7 +195,7 @@ public class P2_Forsaken_beta : SplatoonScript
     private string _lastInstructionLog = "";
 
     public override HashSet<uint>? ValidTerritories { get; } = [TerritoryDancingMadUltimate];
-    public override Metadata Metadata => new(2, "Garume");
+    public override Metadata Metadata => new(3, "Garume");
 
     private Config C => Controller.GetConfig<Config>();
     private IPlayerCharacter BasePlayer => Controller.BasePlayer;

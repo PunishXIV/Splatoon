a18525e019eb1877a0b11b2d6a1f098eefaa403e
nightmarexivplugins@gmail.com <5073202+Limiana@users.noreply.github.com>
2026-08-01 03:28:06 +0300

    Update script

--- SplatoonScripts/Duties/Dawntrail/Dancing Mad/P5_Stray_Apocalypse_Melee.cs
+++ SplatoonScripts/Duties/Dawntrail/Dancing Mad/P5_Stray_Apocalypse_Melee.cs
@@ -4,6 +4,7 @@ using ECommons.GameFunctions;
 using ECommons.ImGuiMethods;
 using ECommons.Logging;
 using FFXIVClientStructs.FFXIV.Client.Game;
+using FFXIVClientStructs.FFXIV.Client.UI;
 using Splatoon.Data;
 using Splatoon.Memory;
 using Splatoon.SplatoonScripting;
@@ -14,7 +15,7 @@ using System.Text;
 
 namespace SplatoonScriptsOfficial.Duties.Dawntrail.Dancing_Mad;
 
-public class P5_Stray_Apocalypse_Melee : SplatoonScript
+public class P5_Stray_Apocalypse_Melee : SplatoonScript<P5_Stray_Apocalypse_Melee.Config>
 {
     public override Metadata Metadata { get; } = new(1, "NightmareXIV");
     public override HashSet<uint>? ValidTerritories { get; } = [1363];
@@ -49,6 +50,12 @@ public class P5_Stray_Apocalypse_Melee : SplatoonScript
             {"Name":"nw right + ne left","refX":100.0,"refY":95.0,"radius":0.5,"color":3356884736,"fillIntensity":0.5,"thicc":4.0}
             {"Name":"nw right + ne mid","refX":105.0,"refY":100.0,"radius":0.5,"color":3356884736,"fillIntensity":0.5,"thicc":4.0}
             {"Name":"nw right + ne right","refX":105.0,"refY":100.0,"radius":0.5,"color":3356884736,"fillIntensity":0.5,"thicc":4.0}
+            {"Name":"nw mid","type":2,"Enabled":false,"refX":80.0,"refY":80.0,"offX":120.0,"offY":120.0,"radius":4.5,"Filled":false,"fillIntensity":0.345,"thicc":6.0}
+            {"Name":"ne right","type":2,"Enabled":false,"refX":120.0,"refY":90.0,"offX":90.0,"offY":120.0,"radius":4.5,"Filled":false,"fillIntensity":0.345,"thicc":6.0}
+            {"Name":"nw left","type":2,"Enabled":false,"refX":80.0,"refY":90.0,"offX":110.0,"offY":120.0,"radius":4.5,"Filled":false,"fillIntensity":0.345,"thicc":6.0}
+            {"Name":"ne left","type":2,"Enabled":false,"refX":110.0,"refY":80.0,"offX":80.0,"offY":110.0,"radius":4.5,"Filled":false,"fillIntensity":0.345,"thicc":6.0}
+            {"Name":"nw right","type":2,"Enabled":false,"refX":90.0,"refY":80.0,"offX":120.0,"offY":110.0,"radius":4.5,"Filled":false,"fillIntensity":0.345,"thicc":6.0}
+            {"Name":"ne left","type":2,"Enabled":false,"refX":110.0,"refY":80.0,"offX":80.0,"offY":110.0,"radius":4.5,"Filled":false,"fillIntensity":0.345,"thicc":6.0}
             
             """);
     }
@@ -58,38 +65,91 @@ public class P5_Stray_Apocalypse_Melee : SplatoonScript
         Controller.Hide();
         var primaryShown = false;
         var secondaryShown = false;
-        for(int i = 0; i < Math.Min(ObservedExasNe.Count, ObservedExasNw.Count); i++)
+        (ExaType, long)? ne = null;
+        (ExaType, long)? nw = null;
+        if(C.OneMode)
         {
-            var ne = ObservedExasNe[i];
-            var nw = ObservedExasNw[i];
-            if(Environment.TickCount64 - ne.Item2 > 7000 || Environment.TickCount64 - nw.Item2 > 7000) continue;
-            var name = $"nw {nw.Item1.ToString().ToLower()} + ne {ne.Item1.ToString().ToLower()}";
-            if(!primaryShown)
+            for(int i = 0; i < ObservedExasNe.Count; i++)
             {
-                primaryShown = true;
-                if(Controller.TryGetElementByName(name, out var e))
+                var cand = ObservedExasNe[i];
+                if(Environment.TickCount64 - cand.Item2 < C.TimeStart) continue;
+                if(Environment.TickCount64 - cand.Item2 > C.TimeEnd) continue;
+                ne = cand;
+                break;
+            }
+            for(int i = 0; i < ObservedExasNw.Count; i++)
+            {
+                var cand = ObservedExasNw[i];
+                if(Environment.TickCount64 - cand.Item2 < C.TimeStart) continue;
+                if(Environment.TickCount64 - cand.Item2 > C.TimeEnd) continue;
+                nw = cand;
+                break;
+            }
+        }
+        else
+        {
+            for(int i = 0; i < Math.Min(ObservedExasNe.Count, ObservedExasNw.Count); i++)
+            {
+                var neCandidate = ObservedExasNe[i];
+                var nwCandidate = ObservedExasNw[i];
+                if(Environment.TickCount64 - neCandidate.Item2 < C.TimeStart || Environment.TickCount64 - nwCandidate.Item2 < C.TimeStart) continue;
+                if(Environment.TickCount64 - neCandidate.Item2 > C.TimeEnd || Environment.TickCount64 - nwCandidate.Item2 > C.TimeEnd) continue;
+                ne = neCandidate;
+                nw = nwCandidate;
+                break;
+            }
+        }
+        if(C.DisplayLines)
+        {
+            if(nw != null)
+            {
+                if(Controller.TryGetElementByName($"nw {nw.Value.Item1.ToString().ToLower()}", out var e))
                 {
                     e.Enabled = true;
-                    e.color = Controller.AttentionColor;
-                    e.tether = true;
-                }
-                else
-                {
-                    PluginLog.Debug($"No element {name}");
+                    e.color = (nw.Value.Item2 > (ne?.Item2 ?? 0) ? EColor.YellowBright : EColor.RedBright).ToUint();
                 }
             }
-            else if(!secondaryShown)
+            if(ne != null)
             {
-                secondaryShown = true;
-                if(Controller.TryGetElementByName(name, out var e))
+                if(Controller.TryGetElementByName($"ne {ne.Value.Item1.ToString().ToLower()}", out var e))
                 {
                     e.Enabled = true;
-                    e.color = EColor.GreenBright.ToUint();
-                    e.tether = false;
+                    e.color = (ne.Value.Item2 > (nw?.Item2 ?? 0) ? EColor.YellowBright : EColor.RedBright).ToUint();
                 }
-                else
+            }
+        }
+        if(ne != null && nw != null)
+        {
+            if(C.DisplaySpots)
+            {
+                var name = $"nw {nw.Value.Item1.ToString().ToLower()} + ne {ne.Value.Item1.ToString().ToLower()}";
+                if(!primaryShown)
                 {
-                    PluginLog.Debug($"No element {name}");
+                    primaryShown = true;
+                    if(Controller.TryGetElementByName(name, out var e))
+                    {
+                        e.Enabled = true;
+                        e.color = Controller.AttentionColor;
+                        e.tether = true;
+                    }
+                    else
+                    {
+                        PluginLog.Debug($"No element {name}");
+                    }
+                }
+                else if(!secondaryShown)
+                {
+                    secondaryShown = true;
+                    if(Controller.TryGetElementByName(name, out var e))
+                    {
+                        e.Enabled = true;
+                        e.color = EColor.GreenBright.ToUint();
+                        e.tether = false;
+                    }
+                    else
+                    {
+                        PluginLog.Debug($"No element {name}");
+                    }
                 }
             }
         }
@@ -103,6 +163,13 @@ public class P5_Stray_Apocalypse_Melee : SplatoonScript
 
     public override void OnSettingsDraw()
     {
+        ImGui.SetNextItemWidth(200);
+        ImGuiEx.SliderIntAsFloat("Start displaying at, seconds", ref C.TimeStart, 0, 3000);
+        ImGui.SetNextItemWidth(200);
+        ImGuiEx.SliderIntAsFloat("Stop displaying at, seconds", ref C.TimeEnd, 5000, 10000);
+        ImGui.Checkbox("Swap at every exaflare (otherwise at every 2)", ref C.OneMode);
+        ImGui.Checkbox("Display safe spots", ref C.DisplaySpots);
+        ImGui.Checkbox("Display safe corridors", ref C.DisplayLines);
         if(ImGui.CollapsingHeader("Debug"))
         {
             ImGuiEx.Text($"{ObservedExasNe.Print()}");
@@ -129,5 +196,14 @@ public class P5_Stray_Apocalypse_Melee : SplatoonScript
                 }
             }
         }
+    }
+
+    public class Config
+    {
+        public int TimeStart = 0;
+        public int TimeEnd = 7000;
+        public bool DisplayLines = false;
+        public bool DisplaySpots = true;
+        public bool OneMode = false;
     }
 }

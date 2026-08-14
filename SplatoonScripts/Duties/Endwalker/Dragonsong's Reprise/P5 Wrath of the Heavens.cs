b266dd72ba14a263bc033eebd2a8573ff0b8d7fa
nightmarexivplugins@gmail.com <5073202+Limiana@users.noreply.github.com>
2026-08-14 06:05:26 +0300

    Update script

--- SplatoonScripts/Duties/Endwalker/Dragonsong's Reprise/P5 Wrath of the Heavens.cs
+++ SplatoonScripts/Duties/Endwalker/Dragonsong's Reprise/P5 Wrath of the Heavens.cs
@@ -1,21 +1,20 @@
+using Dalamud.Bindings.ImGui;
 using Dalamud.Game.ClientState.Objects.SubKinds;
 using Dalamud.Game.ClientState.Objects.Types;
 using ECommons;
 using ECommons.Configuration;
 using ECommons.DalamudServices;
+using ECommons.DalamudServices.Legacy;
 using ECommons.GameFunctions;
 using ECommons.Hooks;
-using Dalamud.Bindings.ImGui;
+using ECommons.ImGuiMethods;
 using Splatoon;
-using ECommons.DalamudServices.Legacy;
 using Splatoon.SplatoonScripting;
+using Splatoon.Utility;
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
 using System.Threading.Tasks;
-using ECommons.ImGuiMethods;
-
-using ECommons.DalamudServices.Legacy;
 
 namespace SplatoonScriptsOfficial.Duties.Endwalker.Dragonsong_s_Reprise;
 
@@ -40,7 +39,7 @@ public class P5_Wrath_of_the_Heavens : SplatoonScript
     private Element? _vellguineTargetElement;
     public override HashSet<uint>? ValidTerritories => [968];
 
-    public override Metadata? Metadata => new(5, "Enthusiastus, Garume, damolitionn");
+    public override Metadata? Metadata => new(6, "Enthusiastus, Garume, damolitionn");
 
     private IBattleNpc? Ignasse =>
         Svc.Objects.FirstOrDefault(x => x is IBattleNpc b && b.DataId == IgnasseDataId) as IBattleNpc;
@@ -48,13 +47,7 @@ public class P5_Wrath_of_the_Heavens : SplatoonScript
     private IBattleNpc? Vellguine =>
         Svc.Objects.FirstOrDefault(x => x is IBattleNpc b && b.DataId == VellguineDataId) as IBattleNpc;
 
-    private IPlayerCharacter PC =>
-        !string.IsNullOrWhiteSpace(TestOverride) &&
-        FakeParty.Get().FirstOrDefault(x => x.Name.TextValue == TestOverride) is IPlayerCharacter pc
-            ? pc
-            : Svc.ClientState.LocalPlayer!;
-
-    private Config Conf => Controller.GetConfig<Config>();
+    private IPlayerCharacter PC => BasePlayer;
 
     public override void OnSetup()
     {
@@ -86,18 +79,39 @@ public class P5_Wrath_of_the_Heavens : SplatoonScript
         _vellguineTargetElement.Enabled = false;
         _vellguineHitboxElement = Controller.RegisterElementFromCode("vellguinehitbox", vellguineHitbox);
         _vellguineHitboxElement.Enabled = false;
-    }
 
+        Controller.RegisterElementFromCode("""
+            {"Name":"Pointer","refX":93.88083,"refY":122.337746,"refZ":-1.9073485E-06,"radius":0.5,"Filled":false,"fillIntensity":0.5,"thicc":5.0,"tether":true}
+            """);
+        Controller.RegisterLayoutFromCode("""
+            ~Lv2~{"Name":"DsrIgnasseTetherCapture","ZoneLockH":[968],"ElementsL":[{"Name":"Pos","type":1,"offX":15.86,"offY":32.66,"color":3356425984,"fillIntensity":0.5,"refActorNPCNameID":3638,"refActorComparisonType":6,"includeRotation":true,"tether":true,"IsCapturing":true,"Nodraw":true}]}
+            """);
+        Controller.RegisterLayoutFromCode("""
+            ~Lv2~{"Name":"DsrVellguineTetherCapture","ZoneLockH":[968],"ElementsL":[{"Name":"Pos","type":1,"offX":5.34,"offY":40.6,"color":3356425984,"fillIntensity":0.5,"refActorNPCNameID":3638,"refActorComparisonType":6,"includeRotation":true,"tether":true,"IsCapturing":true,"Nodraw":true}]}
+            """);
+        Controller.RegisterLayoutFromCode("""
+            ~Lv2~{"Name":"DsrDefamationCapture","ZoneLockH":[968],"Nodraw":true,"ElementsL":[{"Name":"Pos","Nodraw":true,"type":1,"offX":18.64,"offY":17.7,"color":3356425984,"fillIntensity":0.5,"refActorNPCNameID":3638,"refActorComparisonType":6,"includeRotation":true,"tether":true,"IsCapturing":true}]}
+            """);
+        Controller.Hide(layouts: false);
+    }
 
     public override void OnStartingCast(uint source, uint castId)
     {
-        if(castId == 27529) _active = true;
-        if(castId == 27538) _active = false;
+        if(castId == 27529)
+        {
+            _active = true;
+        }
+
+        if(castId == 27538)
+        {
+            _active = false;
+        }
     }
 
     public override void OnVFXSpawn(uint target, string vfxPath)
     {
         if(vfxPath == "vfx/lockon/eff/m0005sp_19o0t.avfx")
+        {
             if(target.TryGetObject(out var pv) && pv is IPlayerCharacter pvc)
             {
                 //DuoLog.Information($"Local player is {PC.Name}");
@@ -105,35 +119,59 @@ public class P5_Wrath_of_the_Heavens : SplatoonScript
                 {
                     //DuoLog.Information($"Skyward Leap is on me, tether other side");
                     _skydiveTargetElement.Enabled = true;
+
+                    var pos = this.Pos["DsrDefamationCapture"].Value;
+                    Controller.Schedule(() =>
+                    {
+                        Controller.GetElementByName("Pointer").Enabled = true;
+                        Controller.GetElementByName("Pointer").SetRefPosition(pos);
+                        Controller.Schedule(() => Controller.GetElementByName("Pointer").Enabled = false, 6200);
+                    }, 6000);
                 }
                 else
                 {
                     //DuoLog.Information($"Skyward Leap is on someone else tether side");
                     if(_gottether)
+                    {
                         return;
+                    }
+
                     _noSkydiveTargetElement.Enabled = true;
                 }
 
-                Task.Delay(8000).ContinueWith(_ =>
+                Controller.Schedule(() =>
                 {
                     _skydiveTargetElement.Enabled = false;
+                }, 6200);
+                Controller.Schedule(() =>
+                {
                     _noSkydiveTargetElement.Enabled = false;
-                });
+                }, 8000);
             }
+        }
 
         if(vfxPath == "vfx/lockon/eff/bahamut_wyvn_glider_target_02tm.avfx")
+        {
             if(target.TryGetObject(out var pv) && pv is IPlayerCharacter pvc && pvc == PC)
             {
                 //DuoLog.Information($"Oh no BahamutWYVNGLIDER on {pvc}");
                 _bahamutDiveTargetElement.Enabled = true;
-                Task.Delay(10000).ContinueWith(_ => { _bahamutDiveTargetElement.Enabled = false; });
+
+                Controller.Schedule(() => { _bahamutDiveTargetElement.Enabled = false;
+
+                }, 10000);
             }
+        }
     }
 
     public override void OnTetherCreate(uint source, uint target, uint data2, uint data3, uint data5)
     {
         // Look for tethers only in p5 wrath (see OnMessage)
-        if(!_active) return;
+        if(!_active)
+        {
+            return;
+        }
+
         if(source.TryGetObject(out var ignasse) && ignasse is IBattleChara ig && ig.NameId == 3638 &&
             target.TryGetObject(out var pi) && pi is IPlayerCharacter pic)
         {
@@ -145,14 +183,22 @@ public class P5_Wrath_of_the_Heavens : SplatoonScript
                 _noSkydiveTargetElement.Enabled = false;
                 _skydiveTargetElement.Enabled = false;
                 _ignasseTargetElement.Enabled = true;
-                Task.Delay(6200).ContinueWith(_ => { _ignasseTargetElement.Enabled = false; });
+                var pos = this.Pos["DsrIgnasseTetherCapture"].Value;
+                Controller.Schedule(() => 
+                { 
+                    _ignasseTargetElement.Enabled = false;
+                    Controller.GetElementByName("Pointer").Enabled = true;
+                    Controller.GetElementByName("Pointer").SetRefPosition(pos);
+                    Controller.Schedule(() => Controller.GetElementByName("Pointer").Enabled = false, 6000);
+                }, 6200);
+
             }
             else
             {
                 _ignasseHitboxElement.SetRefPosition(ignasse.Position);
                 _ignasseHitboxElement.SetOffPosition(_ignassePlayer.Position);
                 _ignasseHitboxElement.Enabled = true;
-                Task.Delay(7000).ContinueWith(_ => { _ignasseHitboxElement.Enabled = false; });
+                Controller.Schedule(() => { _ignasseHitboxElement.Enabled = false; }, 7000);
             }
         }
         else if(source.TryGetObject(out var vellguine) && vellguine is IBattleChara vg && vg.NameId == 3636 &&
@@ -166,42 +212,47 @@ public class P5_Wrath_of_the_Heavens : SplatoonScript
                 _noSkydiveTargetElement.Enabled = false;
                 _skydiveTargetElement.Enabled = false;
                 _vellguineTargetElement.Enabled = true;
-                Task.Delay(6200).ContinueWith(_ => { _vellguineTargetElement.Enabled = false; });
+
+                var pos = this.Pos["DsrVellguineTetherCapture"].Value;
+                Controller.Schedule(() => { 
+                    _vellguineTargetElement.Enabled = false;
+                    Controller.GetElementByName("Pointer").Enabled = true;
+                    Controller.GetElementByName("Pointer").SetRefPosition(pos);
+                    Controller.Schedule(() => Controller.GetElementByName("Pointer").Enabled = false, 6000);
+                }, 6200);
             }
             else
             {
                 _vellguineHitboxElement.SetRefPosition(vellguine.Position);
                 _vellguineHitboxElement.SetOffPosition(_vellguinePlayer.Position);
                 _vellguineHitboxElement.Enabled = true;
-                Task.Delay(7000).ContinueWith(_ => { _vellguineHitboxElement.Enabled = false; });
+                Controller.Schedule(() => { _vellguineHitboxElement.Enabled = false; }, 7000);
             }
         }
     }
-
 
     private void Off()
     {
         _active = false;
         _gottether = false;
-        if(_skydiveTargetElement != null)
-            _skydiveTargetElement.Enabled = false;
-        if(_noSkydiveTargetElement != null)
-            _noSkydiveTargetElement.Enabled = false;
-        if(_bahamutDiveTargetElement != null)
-            _bahamutDiveTargetElement.Enabled = false;
-        if(_ignasseTargetElement != null)
-            _ignasseTargetElement.Enabled = false;
-        if(_vellguineTargetElement != null)
-            _vellguineTargetElement.Enabled = false;
-        if(_ignasseHitboxElement != null)
-            _ignasseHitboxElement.Enabled = false;
-        if(_vellguineHitboxElement != null)
-            _vellguineHitboxElement.Enabled = false;
+        _skydiveTargetElement?.Enabled = false;
+        _noSkydiveTargetElement?.Enabled = false;
+        _bahamutDiveTargetElement?.Enabled = false;
+        _ignasseTargetElement?.Enabled = false;
+        _vellguineTargetElement?.Enabled = false;
+        _ignasseHitboxElement?.Enabled = false;
+        _vellguineHitboxElement?.Enabled = false;
     }
 
+
+    Dictionary<string, Vector3?> Pos = [];
     public override void OnUpdate()
     {
-        Controller.GetRegisteredElements().Where(x => x.Value.tether).Each(x => x.Value.color = GradientColor.Get(EColor.GreenBright, EColor.RedBright, 500).ToUint());
+        foreach(var x in Controller.GetRegisteredLayouts())
+        {
+            Pos[x.Key] = x.Value.GetCapturedPositions()?.SafeSelect("Pos")?.SafeSelect(0) ?? null;
+        }
+        Controller.GetRegisteredElements().Where(x => x.Value.tether).Each(x => x.Value.color = Controller.AttentionColor);
         if(_ignasseHitboxElement.Enabled)
         {
             _ignasseHitboxElement.SetRefPosition(Ignasse.Position);
@@ -218,20 +269,9 @@ public class P5_Wrath_of_the_Heavens : SplatoonScript
     public override void OnDirectorUpdate(DirectorUpdateCategory category)
     {
         if(category.EqualsAny(DirectorUpdateCategory.Commence, DirectorUpdateCategory.Recommence,
-                DirectorUpdateCategory.Wipe)) Off();
-    }
-
-    public static unsafe Vector4 Vector4FromRGBA(uint col)
-    {
-        var bytes = (byte*)&col;
-        return new Vector4(bytes[3] / 255f, bytes[2] / 255f, bytes[1] / 255f, bytes[0] / 255f);
-    }
-
-    public class Config : IEzConfig
-    {
-        public Vector4 ColDoom = Vector4FromRGBA(0x0000ffC8);
-        public Vector4 ColNoDoom = Vector4FromRGBA(0xFF0000C8);
-        public float offZ = 1.8f;
-        public float tScale = 7f;
+                DirectorUpdateCategory.Wipe))
+        {
+            Off();
+        }
     }
 }
